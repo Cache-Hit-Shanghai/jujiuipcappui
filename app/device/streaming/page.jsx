c3b0d749b451bde4d16c06418405e7aa57b0b63b
Jim Jin <jim_king_2000@163.com>
2023-06-25 16:26:52 +0800

    finish record tab

--- app/device/streaming/page.jsx
+++ app/device/streaming/page.jsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { Grommet, Text, Box, Video as VideoCanvas, Nav, Tabs, Tab, CardBody, CheckBox, Main } from 'grommet';
-import { Camera, Video, Microphone, ClosedCaption } from 'grommet-icons';
+import { Grommet, Text, Box, Video as VideoCanvas, Nav, Tabs, Tab, DateInput, RadioButtonGroup, Main, Grid } from 'grommet';
+import { Camera, Video, Microphone, ClosedCaption, Cloud, Sd } from 'grommet-icons';
+import { useState } from 'react';
 import { IconBack, IconButton, PanControl } from '../../../components/Components';
 
 export default function Page() {
+  const [value, setValue] = useState(Date.now());
   return (
     <Grommet full>
       <Box fill gap='medium'>
@@ -35,8 +37,37 @@ export default function Page() {
                 </Box>
               </Box>
             </Tab>
-            <Tab title='录像'></Tab>
-            <Tab title='消息'></Tab>
+            <Tab title='录像'>
+              <Box fill pad='small' gap='small'>
+                <Box direction='row' align='center' justify='between' flex={false}>
+                  <Box direction='row' align='center' background='active' round='xsmall'>
+                    <Text margin={{ left: 'small' }} weight='bold'>{value && new Date(value).toLocaleDateString()}</Text>
+                    <DateInput value={value} onChange={(e) => setValue(e.value)} />
+                  </Box>
+                  <Box round='xlarge' background='active'>
+                    <RadioButtonGroup name='storagetype' direction='row' options={['Cloud', 'SDCard']}>
+                      {(option, { checked, focus, hover }) => {
+                        const Icon = option === 'Cloud' ? Cloud : Sd;
+                        let background;
+                        if (checked) background = 'brand';
+                        else if (hover) background = 'light-4';
+                        else if (focus) background = 'light-4';
+                        return (
+                          <Box background={background} round='full' pad='small'>
+                            <Icon />
+                          </Box>
+                        );
+                      }}
+                    </RadioButtonGroup>
+                  </Box>
+                </Box>
+                <Box flex={{ grow: 1, shrink: 1 }} overflow='auto'>
+                  <Grid fill columns='xsmall' gap='small' align='center' justify='center' style={{ gridAutoFlow: 'row dense', gridAutoRows: '70px' }}>
+                    {Array.from({ length: 37 }, (_, index) => index).map(item => <Box key={item} fill background='black' round='xsmall' />)}
+                  </Grid>
+                </Box>
+              </Box>
+            </Tab>
           </Tabs>
         </Main>
       </Box>

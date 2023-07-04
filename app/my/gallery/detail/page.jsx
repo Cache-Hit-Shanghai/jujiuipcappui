e40052747cdda130c73a934b94de3ae923b300d7
Jim Jin <jim_king_2000@163.com>
2023-07-04 18:17:42 +0800

    trivial change

--- app/my/gallery/detail/page.jsx
+++ app/my/gallery/detail/page.jsx
@@ -14,11 +14,11 @@ export default function Page() {
       <JuJiuMain>
         <Box flex={{ grow: 1, shrink: 1 }} overflow='auto'>
           <Box fill background={{ image: 'url(https://v2.grommet.io/assets/IMG_4245.jpg)', size: 'contain' }} />
-          <Box direction='row' justify='evenly' background='background-contrast' flex={false}>
-            <IconButton icon={<Download />} label='下载' onClick={() => {}} />
-            <IconButton icon={<ShareRounded />} label='分享' onClick={() => {}} />
-            <IconButton icon={<Trash />} label='删除' onClick={() => {}} />
-          </Box>
+        </Box>
+        <Box direction='row' justify='evenly' background='background-contrast' flex={false}>
+          <IconButton icon={<Download />} label='下载' onClick={() => {}} />
+          <IconButton icon={<ShareRounded />} label='分享' onClick={() => {}} />
+          <IconButton icon={<Trash />} label='删除' onClick={() => {}} />
         </Box>
       </JuJiuMain>
     </Box>

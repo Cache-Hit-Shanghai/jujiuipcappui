3dd12771e41933df50079de4ae8424fa1a590adb
Jim Jin <jim_king_2000@163.com>
2023-07-04 17:34:50 +0800

    add gallery detail

---
+++ app/my/gallery/detail/page.jsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Box, Nav } from 'grommet';
+import { Trash, Share, Download } from 'grommet-icons';
+import { IconBack, JuJiuMain, IconButton } from '../../../../components/Components';
+
+
+export default function Page() {
+  return (
+    <Box fill>
+      <Nav direction='row' align='center'>
+        <IconBack />
+      </Nav>
+      <JuJiuMain>
+        <Box flex={{ grow: 1, shrink: 1 }} overflow='auto'>
+          <Box fill background={{ image: 'url(https://v2.grommet.io/assets/IMG_4245.jpg)', size: 'contain' }} />
+          <Box direction='row' justify='evenly' background='background-contrast' flex={false}>
+            <IconButton icon={<Download />} label='下载' onClick={() => {}} />
+            <IconButton icon={<Share />} label='分享' onClick={() => {}} />
+            <IconButton icon={<Trash />} label='删除' onClick={() => {}} />
+          </Box>
+        </Box>
+      </JuJiuMain>
+    </Box>
+  );
+}
--- app/my/gallery/page.jsx
+++ app/my/gallery/page.jsx
@@ -1,10 +1,13 @@
 'use client';
 
 import { Text, Box, Nav, Grid } from 'grommet';
+import { useRouter } from 'next/navigation';
 import { IconBack, JuJiuMain } from '../../../components/Components';
 
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <Box fill>
       <Nav direction='row' align='center'>
@@ -14,7 +17,8 @@ export default function Page() {
       <JuJiuMain>
         <Box flex={{ grow: 1, shrink: 1 }} overflow='auto'>
           <Grid fill columns='xsmall' gap='small' align='center' justify='center' style={{ gridAutoFlow: 'row dense', gridAutoRows: '70px' }}>
-            {Array.from({ length: 37 }, (_, index) => index).map(item => <Box key={item} fill background='url(https://v2.grommet.io/assets/IMG_4245.jpg)' round='xsmall' />)}
+            {Array.from({ length: 37 }, (_, index) => index).map(
+              item => <Box key={item} fill background='url(https://v2.grommet.io/assets/IMG_4245.jpg)' round='xsmall' onClick={() => router.push('/my/gallery/detail')} />)}
           </Grid>
         </Box>
       </JuJiuMain>

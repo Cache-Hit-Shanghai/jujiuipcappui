6f44ed81dcf09bcdd2b9569465608e6e08eee034
Jim Jin <jim_king_2000@163.com>
2023-07-10 16:48:36 +0800

    trivial change

--- app/device/scan/page.jsx
+++ app/device/scan/page.jsx
@@ -18,7 +18,26 @@ export default function Page() {
 				<Text>扫一扫</Text>
 			</Nav>
       <JuJiuMain>
-        <Box flex={{ grow: 1, shrink: 1 }} border></Box>
+        <Box flex={{ grow: 1, shrink: 1 }} style={{ position: 'relative' }} background={{
+          image: 'linear-gradient(to bottom, transparent 19px, rgba(255, 255, 255, 0.1) 20px), linear-gradient(to right, transparent 19px, rgba(255, 255, 255, 0.1) 20px)',
+          size: '20px 20px',
+          repeat: 'repeat',
+        }}>
+          <Box width='70%' height='70%' margin='auto'
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+            }}
+          >
+            <Box width='40px' height='40px' border={{ color: 'green', size: 'medium', side: 'topright' }} style={{ position: 'absolute', top: 0 }} />
+            <Box width='40px' height='40px' border style={{ position: 'absolute', right: 0 }} />
+            <Box width='40px' height='40px' border style={{ position: 'absolute', bottom: 0 }} />
+            <Box width='40px' height='40px' border style={{ position: 'absolute', right: 0, bottom: 0 }} />
+          </Box>
+        </Box>
         <Box direction='row' justify='evenly' margin={{ vertical: 'large' }}>
           <Avatar border>
             <Torch size='24' />

3df5bd41d01ec15e42b6b466070e85c4a9322f64
Jim Jin <jim_king_2000@163.com>
2023-07-13 12:58:35 +0800

    change the background color for qrcode sharing

--- app/device/sharing/qrcode/page.jsx
+++ app/device/sharing/qrcode/page.jsx
@@ -13,7 +13,7 @@ export default function Page() {
         <Text>二维码分享</Text>
       </Nav>
       <JuJiuMain align='center'>
-        <Box width='medium' height='medium' background='background-contrast' align='center' justify='center'>
+        <Box width='medium' height='medium' background='light-6' align='center' justify='center'>
           <Qr color='plain' size='xlarge' />
         </Box>
         <Text size='small'>扫一扫上面的二维码图案，接受分享的设备。</Text>

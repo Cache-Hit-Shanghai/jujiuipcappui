4b1f9f7c7ddfeea222788a9a0a18689bc65a159a
Jim Jin <jim_king_2000@163.com>
2023-07-17 18:46:36 +0800

    trivial change

--- app/my/uisettings/page.jsx
+++ app/my/uisettings/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Text, Box, RadioButtonGroup, Nav, CardHeader, CardBody } from 'grommet';
+import { Text, Box, RadioButtonGroup, Nav, Card, CardHeader, CardBody } from 'grommet';
 import { IconBack, JuJiuMain, JuJiuCard } from '../../../components/Components';
 
 export default function Page() {
@@ -10,33 +10,33 @@ export default function Page() {
         <IconBack />
         <Text>界面设置</Text>
       </Nav>
-      <JuJiuMain>
-        <JuJiuCard>
-          <CardHeader>
+      <JuJiuMain gap='medium'>
+        <Card>
+          <CardHeader pad='small' background='background-front'>
             <Text weight='bold'>语言</Text>
           </CardHeader>
-          <CardBody>
+          <CardBody pad='small' background='background-contrast'>
             <RadioButtonGroup name='language' options={['简体中文', '繁体中文', 'English']} value='简体中文' />
           </CardBody>
-        </JuJiuCard>
-        <JuJiuCard>
-          <CardHeader>
+        </Card>
+        <Card>
+          <CardHeader pad='small' background='background-front'>
             <Text weight='bold'>字体大小</Text>
           </CardHeader>
-          <CardBody>
+          <CardBody pad='small' background='background-contrast'>
             <RadioButtonGroup name='fontsize' options={['普通', '大字体']} value='普通' />
           </CardBody>
-        </JuJiuCard>
-        <JuJiuCard>
-          <CardHeader>
+        </Card>
+        <Card>
+          <CardHeader pad='small' background='background-front'>
             <Text weight='bold'>深色模式</Text>
           </CardHeader>
-          <CardBody>
+          <CardBody pad='small' background='background-contrast'>
             <Box direction='row' justify='between'>
               <RadioButtonGroup name='language' options={['浅色模式', '深色模式', '跟随系统']} value='深色模式' />
             </Box>
           </CardBody>
-        </JuJiuCard>
+        </Card>
       </JuJiuMain>
     </Box>
   );

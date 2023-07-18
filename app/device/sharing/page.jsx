95a387a3a18fd76d806d704d5617a952dfe79b87
Jim Jin <jim_king_2000@163.com>
2023-07-18 15:03:29 +0800

    modify device sharing page

--- app/device/sharing/page.jsx
+++ app/device/sharing/page.jsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Text, Box, Nav, Layer, Button } from 'grommet';
+import { Text, Box, Nav, Layer, Button, CardBody } from 'grommet';
+import { FormNext } from 'grommet-icons';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { IconBack, ButtonGroup, JuJiuMain } from '../../../components/Components';
+import { IconBack, ButtonGroup, JuJiuMain, JuJiuCard } from '../../../components/Components';
 
 
 export default function Page() {
@@ -21,7 +22,27 @@ export default function Page() {
         <Text>设备分享 - 办3</Text>
       </Nav>
       <JuJiuMain>
+        <JuJiuCard>
+          <CardBody>
+            <Text>分享好友(0)</Text>
+          </CardBody>
+        </JuJiuCard>
+        <JuJiuCard>
+          <CardBody>
+            <Box direction='row' align='center' justify='between'>
+              <Box>
+                <Text>扩容服务</Text>
+                <Text size='small' color='text-xweak'>未开通：可免费分享5人</Text>
+              </Box>
+              <Box direction='row'>
+                <Text>去扩容</Text>
+								<FormNext color='control' />
+              </Box>
+            </Box>
+          </CardBody>
+        </JuJiuCard>
         <ButtonGroup data={buttons} />
+        <Button label='取消所有分享' />
         {openWechatSharing && (
           <Layer position='bottom' full='horizontal' onClickOutside={() => setOpenWechatSharing(false)} responsive={false}>
             <Box fill border pad='medium' gap='medium'>

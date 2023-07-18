c4c2ddbf793897d766dfa4a9bad9c12f68fb1b7a
Jim Jin <jim_king_2000@163.com>
2023-07-18 11:10:10 +0800

    add new post

---
+++ app/my/feedback/new/page.jsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { Text, Box, TextInput , Nav, TextArea, CardBody, Markdown, Heading, Button } from 'grommet';
+import { IconBack, JuJiuMain } from '../../../../components/Components';
+
+const content=`
+## 主要步骤如下
+1. 单击“云探”app。
+1. 单击摄像机设置小图标。
+1. 选择“设置”。
+1. 滚动到最下方，选择“转移设备”。
+`;
+
+export default function Page() {
+  return (
+    <Box fill>
+      <Nav direction='row' align='center'>
+        <IconBack />
+        <Text>新建问题</Text>
+      </Nav>
+      <Box margin='small' flex={false}>
+        <TextInput placeholder='标题' />
+      </Box>
+      <JuJiuMain gap='medium'>
+        <TextArea fill placeholder='问题描述' />
+      </JuJiuMain>
+      <Box pad='small' direction='row' flex={false} justify='end'>
+        <Button label='提交' />
+      </Box>
+    </Box>
+  );
+}
--- app/my/feedback/page.jsx
+++ app/my/feedback/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Text, Box, TextInput , Nav, Button, List, Tag, Pagination } from 'grommet';
-import { Search } from 'grommet-icons';
+import { Search, Add } from 'grommet-icons';
 import { Article } from '@styled-icons/remix-fill/Article';
 import Link from "next/link";
 import { IconBack, JuJiuMain } from '../../../components/Components';
@@ -12,42 +12,50 @@ const data = [
   { title: '摄像机硬件如何保养？', createdBy: 'Tony', date: '2023/7/1' },
   { title: '是否可以把摄像机共享给家人？', createdBy: 'Jack', date: '2023/7/1' },
   { title: '一个非常非常长的标题一个非常非常长的标题一个非常非常长的标题', createdBy: 'Lisa', date: '2023/7/1' },
+  { title: '如何使用“设备转移”？', createdBy: 'Jim', date: '2023/7/1' },
+  { title: '云端费用如何计算？', createdBy: 'Jim', date: '2023/7/1' },
+  { title: '摄像机硬件如何保养？', createdBy: 'Tony', date: '2023/7/1' },
+  { title: '是否可以把摄像机共享给家人？', createdBy: 'Jack', date: '2023/7/1' },
+  { title: '一个非常非常长的标题一个非常非常长的标题一个非常非常长的标题', createdBy: 'Lisa', date: '2023/7/1' },
 ];
 
 export default function Page() {
   return (
     <Box fill>
-      <Nav direction='row' align='center'>
-        <IconBack />
-        <Text>帮助与反馈</Text>
+      <Nav direction='row' align='center' justify='between'>
+        <Box direction='row' align='center'>
+          <IconBack />
+          <Text>帮助与反馈</Text>
+        </Box>
+        <Link href='/my/feedback/new' passHref legacyBehavior>
+          <Button as='a' margin='small' size='small' label='新建问题' icon={<Add />} />
+        </Link>
       </Nav>
+      <Box border flex={false} direction='row' align='center' margin={{ horizontal: 'small' }}>
+        <TextInput plain icon={<Search />} placeholder='搜索摄像机……' />
+        <Box flex={false} margin='small'><Button primary size='small' label='搜索' /></Box>
+      </Box>
       <JuJiuMain>
-        <Box direction='row' align='center' gap='small'>
-          <TextInput icon={<Search />} placeholder='搜索……' />
-          <Box flex={false}><Button primary label='新建问题' /></Box>
-        </Box>
-        <Box>
-          <List data={data}>
-            {(datum) => (
-              <Box direction='row' gap='small'>
-                <Box flex={false}><Article size='24' /></Box>
-                <Box gap='small'>
-                  <Link href='/my/feedback/post' passHref legacyBehavior>
-                    <Text truncate color='control'>{datum.title}</Text>
-                  </Link>
-                  <Box direction='row' gap='small'>
-                    <Tag background='graph-4' size='xsmall' value='bug' />
-                    <Tag background='graph-3' size='xsmall' value='建议' />
-                    <Tag background='graph-2' size='xsmall' value='功能' />
-                    <Tag background='graph-1' size='xsmall' value='文档' />
-                    <Tag background='graph-0' size='xsmall' value='帮助' />
-                  </Box>
-                  <Text size='small' color='text-xweak'>由{datum.createdBy}于{datum.date}创建</Text>
+        <List data={data} margin={{ vertical: 'small' }}>
+          {(datum) => (
+            <Box direction='row' gap='small'>
+              <Box flex={false}><Article size='24' /></Box>
+              <Box gap='small'>
+                <Link href='/my/feedback/post' passHref legacyBehavior>
+                  <Text truncate color='control'>{datum.title}</Text>
+                </Link>
+                <Box direction='row' gap='small'>
+                  <Tag background='graph-4' size='xsmall' value='bug' />
+                  <Tag background='graph-3' size='xsmall' value='建议' />
+                  <Tag background='graph-2' size='xsmall' value='功能' />
+                  <Tag background='graph-1' size='xsmall' value='文档' />
+                  <Tag background='graph-0' size='xsmall' value='帮助' />
                 </Box>
+                <Text size='small' color='text-xweak'>由{datum.createdBy}于{datum.date}创建</Text>
               </Box>
-            )}
-          </List>
-        </Box>
+            </Box>
+          )}
+        </List>
       </JuJiuMain>
       <Box align='center' pad='small'>
         <Pagination numberItems={237} />

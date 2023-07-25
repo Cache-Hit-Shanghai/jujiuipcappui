1a372630234896572c63751ca5f804884795dfb7
Jim Jin <jim_king_2000@163.com>
2023-07-25 14:15:24 +0800

    apply JuJiuInformation

--- app/my/ota/updating/page.jsx
+++ app/my/ota/updating/page.jsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { Text, Box, Nav, Meter, Heading } from 'grommet';
-import { CircleInformation } from 'grommet-icons';
+import { Text, Box, Nav, Meter, Heading, Avatar } from 'grommet';
+import { Webcam } from 'grommet-icons';
 import { IconBack, JuJiuMain, ButtonBackToMain } from '../../../../components/Components';
+import { JuJiuInformation } from '../../../../components/JuJiuInformation';
 
 
 export default function Page() {
@@ -14,12 +15,13 @@ export default function Page() {
       </Nav>
       <JuJiuMain>
         <Heading margin='none' level={2}>固件升级中……</Heading>
-        <Box direction='row' gap='small' align='center'>
-          <CircleInformation color='status-warning' />
-          <Text size='small' color='status-warning'>
-            摄像机办3正在固件升级中。请保持摄像机的电源开启。摄像机突然断电有可能导致设备无法启动。
-          </Text>
+        <Box align='center'>
+          <Avatar background='background-front'>
+            <Webcam />
+          </Avatar>
+          <Text>办3</Text>
         </Box>
+        <JuJiuInformation label='摄像机正在固件升级中。请保持摄像机的电源开启。摄像机突然断电有可能导致设备无法启动。' />
         <Meter round values={[{ value: 50 }]} />
         <Box align='center' gap='medium'>
           <Text>固件升级成功！设备正在重启中，请耐心等待。设备重启后即可正常使用。</Text>
--- components/JuJiuInformation.jsx
+++ components/JuJiuInformation.jsx
@@ -1,9 +1,9 @@
 import { Box, Text } from 'grommet';
 import { CircleInformation } from 'grommet-icons';
 
-export function JuJiuInformation({ label, size }) {
+export function JuJiuInformation({ label, size, ...props }) {
   return (
-    <Box direction='row' gap='small' align='center' flex={false} margin={{ horizontal: 'small' }}>
+    <Box direction='row' gap='small' align='center' flex={false} margin={{ horizontal: 'small' }} {...props}>
       <CircleInformation size={size} color='accent-4' />
       <Text size='small' color='accent-4' textAlign='justify'>{label}</Text>
     </Box>

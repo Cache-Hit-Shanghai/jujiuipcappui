f00c4e53f3e0753c628fcca9f93cec6e7d072eb0
Jim Jin <jim_king_2000@163.com>
2023-08-10 14:09:53 +0800

    finish cloud storage packages

--- app/my/cloudstorage/settings/packages/page.jsx
+++ app/my/cloudstorage/settings/packages/page.jsx
@@ -8,6 +8,12 @@ import { IconBack, JuJiuMain } from '../../../../../components/Components';
 import { JuJiuItem, JuJiuItemButton, JuJiuItemLink } from '../../..//../../components/JuJiuItem';
 
 
+const packages = [
+  { title: '7天月套餐', start: '2023/08/10', end: '2023/09/10', current: true },
+  { title: '30天月套餐', start: '2023/09/10', end: '2023/10/10' },
+  { title: '30天半年套餐', start: '2023/10/10', end: '2024/04/10' },
+];
+
 export default function Page() {
   return (
     <Box fill>
@@ -16,7 +22,20 @@ export default function Page() {
         <Text>已购套餐列表</Text>
       </Nav>
       <JuJiuMain gap='medium'>
-        
+        <List data={packages} margin={{ vertical: 'small' }}>
+          {(datum) => (
+            <Box direction='row' gap='small' align='center' justify='between'>
+              <Box direction='row' align='center' gap='small'>
+                <Text>{datum.title}</Text>
+                {datum.current && <Tag size='small' background='status-ok' border={false} value='当前套餐' />}
+              </Box>
+              <Box>
+                <Text size='small' color='text-xweak'>{datum.start}</Text>
+                <Text size='small' color='text-xweak'>{datum.end}</Text>
+              </Box>
+            </Box>
+          )}
+        </List>
       </JuJiuMain>
     </Box>
   );

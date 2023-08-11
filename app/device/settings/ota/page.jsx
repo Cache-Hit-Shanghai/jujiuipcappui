74eaf106887d725e0176c0e24b6e84cf01031d20
Jim Jin <jim_king_2000@163.com>
2023-08-11 17:11:55 +0800

    add ota

---
+++ app/device/settings/ota/page.jsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { Text, Box, Nav, CheckBox } from 'grommet';
+import { IconBack, JuJiuMain } from '../../../../components/Components';
+import { JuJiuItem } from '../../../../components/JuJiuItem';
+
+
+export default function Page() {
+  return (
+    <Box fill>
+      <Nav direction='row' align='center'>
+        <IconBack />
+        <Text>固件升级设置 - 办3</Text>
+      </Nav>
+      <JuJiuMain>
+        <JuJiuItem label='自动升级固件'>
+					<CheckBox toggle />
+				</JuJiuItem>
+      </JuJiuMain>
+    </Box>
+  );
+}

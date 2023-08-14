5839eb9fe4b03faa6bfc5d09d68b60660e47d4aa
Jim Jin <jim_king_2000@163.com>
2023-08-14 12:43:27 +0800

    hide unimplemented pages

--- app/device/settings/ai/page.jsx
+++ app/device/settings/ai/page.jsx
@@ -15,8 +15,8 @@ export default function Page() {
       <JuJiuMain>
         <JuJiuItemLink label='人脸识别' href='/device/settings/ai/face' />
 				<JuJiuItemLink label='人形检测' href='/device/settings/ai/body' />
-				<JuJiuItemLink label='区域看护' href='/device/settings/ai/zone' />
-				<JuJiuItemLink label='时光相册' href='/device/settings/ai/time' />
+				{/* <JuJiuItemLink label='区域看护' href='/device/settings/ai/zone' />
+				<JuJiuItemLink label='时光相册' href='/device/settings/ai/time' /> */}
       </JuJiuMain>
     </Box>
   );

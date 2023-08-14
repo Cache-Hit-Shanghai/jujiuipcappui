67fd19ced540c74d4c452698d8fa2076154130d4
Jim Jin <jim_king_2000@163.com>
2023-08-14 11:58:49 +0800

    add face management page

---
+++ app/device/settings/ai/face/management/page.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Text, Box, Nav, CheckBox, Heading } from 'grommet';
+import { IconBack, JuJiuMain } from '../../../../../components/Components';
+import { JuJiuItem, JuJiuItemLink } from '../../../../../components/JuJiuItem';
+
+
+export default function Page() {
+  return (
+    <Box fill>
+      <Nav direction='row' align='center'>
+        <IconBack />
+        <Text>人脸管理 - 办3</Text>
+      </Nav>
+      <JuJiuMain>
+      </JuJiuMain>
+    </Box>
+  );
+}

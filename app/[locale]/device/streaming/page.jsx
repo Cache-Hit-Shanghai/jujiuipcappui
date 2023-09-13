54e6b1e7b2874601aa6422d457837d329eb8a031
Jim Jin <jim_king_2000@163.com>
2023-09-13 15:11:09 +0800

    add helper text for disconnection

--- app/[locale]/device/streaming/page.jsx
+++ app/[locale]/device/streaming/page.jsx
@@ -8,6 +8,8 @@ import {
 	Tag,
 	Stack,
 	Spinner,
+	Button,
+	Heading,
 } from 'grommet';
 import { RadialSelected, ShareRounded, StatusCritical } from 'grommet-icons';
 import { Settings3 } from '@styled-icons/remix-fill/Settings3';
@@ -23,9 +25,12 @@ import {
 	ZoomControl,
 	PanControl,
 } from '@/jujiuuicomponents/Components';
-import { ButtonLink, IconBack, JuJiuMain, } from '@/jujiuuicomponents/new/core_ui';
+import { ButtonLink, IconBack, JuJiuMain, JuJiuLayer } from '@/jujiuuicomponents/new/core_ui';
+import Link from '@/state/translate';
 
 export function StreamPlayer() {
+	const [openHelp, setOpenHelp] = useState(true);
+
 	return (
 		<Stack interactiveChild='last'>
 			<Box flex={false} background='background-contrast'>
@@ -58,6 +63,25 @@ export function StreamPlayer() {
 					<Spinner size='large' />
 					<StatusCritical size='large' />
 					<Text>无法加载视频流。</Text>
+					<Text>设备已离线，离线时间：2023年9月14日 14:52:30。</Text>
+					<Button primary label='查看帮助' onClick={() => setOpenHelp(true)} />
+					{openHelp && (
+						<JuJiuLayer
+							onClickOutside={() => setOpenHelp(false)}
+						>
+							<Heading
+								level={3}
+								alignSelf='center'
+								margin='none'
+							>
+								帮助
+							</Heading>
+							<Text>1. 请分别检查手机和摄像机是否联网正常，若修改过WiFi名称或密码，请重新连接。</Text>
+							<Text>2. 若手机和摄像机均联网正常，请等待数分钟后再访问。</Text>
+							<Text>3. 请尝试重启路由器。</Text>
+							<Text>4. 如以上均无法解决，请<Link href='/my/feedback' passHref legacyBehavior>反馈问题</Link>。</Text>
+						</JuJiuLayer>
+					)}
 				</Box>
 				<Box align='end'>
 					<ButtonLink href='/device/streaming/fullscreen' icon={<PhoneLandscape size='24' />} />

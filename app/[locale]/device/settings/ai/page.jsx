dd5a5b0341cb1fba44ad37c421e44e572d71eb97
Jim Jin <jim_king_2000@163.com>
2023-09-13 12:45:17 +0800

    remove face detection

--- app/[locale]/device/settings/ai/page.jsx
+++ app/[locale]/device/settings/ai/page.jsx
@@ -13,7 +13,7 @@ export default function Page() {
         <Text>智能检测 - 办3</Text>
       </Nav>
       <JuJiuMain>
-        <JuJiuItemLink label='人脸识别' href='/device/settings/ai/face' />
+        {/* <JuJiuItemLink label='人脸识别' href='/device/settings/ai/face' /> */}
 				<JuJiuItemLink label='人形追踪' href='/device/settings/ai/body' />
 				{/* <JuJiuItemLink label='区域看护' href='/device/settings/ai/zone' />
 				<JuJiuItemLink label='时光相册' href='/device/settings/ai/time' /> */}

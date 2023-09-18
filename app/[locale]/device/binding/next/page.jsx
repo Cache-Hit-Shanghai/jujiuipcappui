0674c6e5853f9ed13ae58db6928d3b34224b559a
Jim Jin <jim_king_2000@163.com>
2023-09-18 11:13:19 +0800

    add import { Qr } from 'grommet-icons';

--- app/[locale]/device/binding/next/page.jsx
+++ app/[locale]/device/binding/next/page.jsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { Text, Box, Nav } from 'grommet';
-import { Qr } from 'grommet-icons';
 import { ButtonBackToMain } from '@/jujiuuicomponents/Components';
-import { JuJiuInformation, IconBack, JuJiuMain } from '@/jujiuuicomponents/core/core_ui';
+import { IconBack, JuJiuMain } from '@/jujiuuicomponents/core/core_ui';
+import { DeviceBinding } from '@/jujiuuicomponents/application/device/binding';
+
 
 export default function Page() {
   return (
@@ -13,10 +14,7 @@ export default function Page() {
         <Text>添加设备</Text>
       </Nav>
       <JuJiuMain align='center'>
-        <Box fill='horizontal' height='medium' background='light-6' align='center' justify='center' flex={false}>
-          <Qr color='plain' size='xlarge' />
-        </Box>
-        <JuJiuInformation size='large' label='请在听到网络摄像机“绑定设备”的提示音后，将该二维码放置在摄像机镜头前约30厘米左右，等待设备“配置成功”的提示音。' />
+        <DeviceBinding />
         <Box>
           <ButtonBackToMain />
         </Box>

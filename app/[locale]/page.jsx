a7554fd5bb23dc9e61c6b3bfe4d3358fd3131676
Jim Jin <jim_king_2000@163.com>
2023-10-11 17:47:49 +0800

    update submodule

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -5,7 +5,7 @@ import { AddCircle, Mail, Add, Scan, CreativeCommons } from 'grommet-icons';
 import { Group } from '@styled-icons/fluentui-system-regular/Group';
 import Link, { useJuJiuT } from '@/state/translate';
 import { AppFooter } from '@/app/components';
-import { IpcCard } from '@/jujiu-ui-components/ipc/video/control';
+import { IpcCard } from '@/jujiu-ui-components/ipc/device/avatar';
 import { ButtonLink, JuJiuMain } from '@/jujiu-ui-components/core/core-ui';
 import { JuJiuTagFromShared, JuJiuTagSharing } from '@/jujiu-ui-components/core/core-tag';
 
@@ -70,6 +70,7 @@ export default function Page() {
 						<Box gap='medium' margin='small'>
 							<IpcCard
 								key={0}
+								type='ipc'
 								label={
 									<Box direction='row' align='center' gap='large'>
 										<Text>客厅IPC</Text>
@@ -81,6 +82,7 @@ export default function Page() {
 							/>
 							<IpcCard
 								key={10}
+								type='robot'
 								label={
 									<Box direction='row' align='center' gap='large'>
 										<Text>PixelBot</Text>
@@ -96,21 +98,25 @@ export default function Page() {
 						<Box gap='medium' margin='small'>
 							<IpcCard
 								key={1}
+								type='ipc'
 								label='办3'
 								imgurl='https://ts1.cn.mm.bing.net/th/id/R-C.4894a961ab87e3459babae4ef8a2f4fa?rik=1P7ZI7Evnz4Pqg&riu=http%3a%2f%2fpic.zsucai.com%2ffiles%2f2013%2f0830%2fxiaguang2.jpg&ehk=Ok%2fjrv35R0L218oT%2flliRL8DJc52pARVnWU%2bXOpUwq4%3d&risl=&pid=ImgRaw&r=0'
 							/>
 							<IpcCard
 								key={2}
+								type='ipc'
 								label='办5'
 								imgurl='https://ts1.cn.mm.bing.net/th/id/R-C.f54c83f04442cec528a250d251251ce6?rik=JE7BoZk5xK4iEg&riu=http%3a%2f%2fpic4.bbzhi.com%2ffengjingbizhi%2fgaoqingxifengjingzhuomianbizhixiazai%2fgaoqingxifengjingzhuomianbizhixiazai_366146_18.jpg&ehk=YvUnl11nBp%2fGJssQUbYqkXLo7fchkD%2fEQ8BGpW2Urjs%3d&risl=&pid=ImgRaw&r=0'
 							/>
 							<IpcCard
 								key={3}
+								type='ipc'
 								label='办7'
 								imgurl='https://ts1.cn.mm.bing.net/th/id/R-C.3edbd350d03c25ed988236c50d0733e6?rik=txi3%2f%2b%2fVYUJofg&riu=http%3a%2f%2fpic.zsucai.com%2ffiles%2f2013%2f0802%2fwmdqfj4.jpg&ehk=TY9%2f90VQn6m3NYCoiPX2UyRYQIT7dkGJtTJli1W7pfo%3d&risl=&pid=ImgRaw&r=0'
 							/>
 							<IpcCard
 								key={4}
+								type='ipc'
 								label='办9'
 								imgurl='https://ts1.cn.mm.bing.net/th/id/R-C.0c8bf36e099654aadaf5f127ef1a3f1b?rik=uHrB%2blGez03%2fAA&riu=http%3a%2f%2fi3.img.969g.com%2fdown%2fimgx2014%2f10%2f24%2f289_102445_a1cff.jpg&ehk=EeF%2fioqRM6NfQqkCgXw%2bwLvO1%2fxZgeZ2pof7ALNLGsg%3d&risl=&pid=ImgRaw&r=0'
 							/>

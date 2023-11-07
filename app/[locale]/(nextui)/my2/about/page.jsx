01506f3319c4be079d6557308591fceb98a37681
Jim <jim_king_2000@163.com>
2023-11-07 23:20:50 +0800

    finish my2/about

--- app/[locale]/(nextui)/my2/about/page.jsx
+++ app/[locale]/(nextui)/my2/about/page.jsx
@@ -1,8 +1,20 @@
+'use client';
+
 import { Navbar, NavbarItem, NavbarContent } from '@nextui-org/react';
-import { ButtonBack } from '@/jujiu-ui-components/nextui/core/core-ui';
+import { ButtonBack, LinkGroup } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { AppLogo, AppMark } from '@/jujiu-ui-components/nextui/ipc/about/brand';
+import { useJuJiuT } from '@/state/translate';
 
 export default function Page() {
+	const t = useJuJiuT();
+	const links = [
+		{ label: t('检查更新'), url: '#' },
+		{ label: t('服务条款'), url: '#' },
+		{ label: t('隐私政策'), url: '#' },
+		{ label: t('官网'), url: '#' },
+		{ label: t('使用帮助'), url: '#' },
+	];
+
 	return (
 		<div className='h-screen flex flex-col'>
 			<Navbar isBordered position='static' height='3rem' maxWidth='full'>
@@ -15,8 +27,9 @@ export default function Page() {
 					</NavbarItem>
 				</NavbarContent>
 			</Navbar>
-			<div className='flex flex-col flex-1 p-2'>
+			<div className='flex flex-col flex-1 p-2 gap-4'>
 				<AppLogo />
+				<LinkGroup data={links} />
 			</div>
 			<AppMark />
 		</div>

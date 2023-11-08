a97b9cdb756d460d2570e8f93f973771c6ceabd4
Jim <jim_king_2000@163.com>
2023-11-08 19:23:48 +0800

    use client router

--- app/[locale]/(nextui)/providers.jsx
+++ app/[locale]/(nextui)/providers.jsx
@@ -2,10 +2,13 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 import { ThemeProvider } from 'next-themes';
+import { useRouter } from '@/state/translate';
 
 export function Providers({ children }) {
+	const router = useRouter();
+
 	return (
-		<NextUIProvider>
+		<NextUIProvider navigate={router.push}>
 			<ThemeProvider attribute='class' defaultTheme='dark'>
 				{children}
 			</ThemeProvider>

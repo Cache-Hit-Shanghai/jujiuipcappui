8ed47812627b78e31ae5cb79add4448ddd5e4a7b
Jim <jim_king_2000@163.com>
2023-11-09 23:10:11 +0800

    refactory

--- app/[locale]/(nextui)/components/index.jsx
+++ app/[locale]/(nextui)/components/index.jsx
@@ -18,7 +18,7 @@ import {
 	ChipCloudStorageExpiring,
 } from '@/jujiu-ui-components/nextui/core/core-chips';
 import Link, { useJuJiuT } from '@/state/translate';
-import { PlusCircleIcon, SquaresPlusIcon, RectangleGroupIcon, QrCodeIcon } from '@heroicons/react/24/solid';
+import { PlusCircleIcon, SquaresPlusIcon, RectangleGroupIcon } from '@heroicons/react/24/solid';
 import { IpcCard, BotCard } from './ipcCard';
 
 export function DeviceMenu() {
@@ -48,14 +48,6 @@ export function DeviceMenu() {
 				>
 					{t('设备分组')}
 				</DropdownItem>
-				<DropdownItem
-					key='scan'
-					as={Link}
-					href='/device/scan'
-					startContent={<QrCodeIcon className='h-6 w-6' />}
-				>
-					{t('扫一扫')}
-				</DropdownItem>
 			</DropdownMenu>
 		</Dropdown>
 	);
@@ -72,14 +64,14 @@ export function DeviceList() {
 						label='办公室0'
 						src='https://ts1.cn.mm.bing.net/th/id/R-C.f54c83f04442cec528a250d251251ce6?rik=JE7BoZk5xK4iEg&riu=http%3a%2f%2fpic4.bbzhi.com%2ffengjingbizhi%2fgaoqingxifengjingzhuomianbizhixiazai%2fgaoqingxifengjingzhuomianbizhixiazai_366146_18.jpg&ehk=YvUnl11nBp%2fGJssQUbYqkXLo7fchkD%2fEQ8BGpW2Urjs%3d&risl=&pid=ImgRaw&r=0'
 						href='/device/streaming'
-						bodyChips={[<ChipOnline />, <ChipCloudStorageExpiring />]}
+						bodyChips={[<ChipOnline key={0} />, <ChipCloudStorageExpiring key={1} />]}
 						footerChips={[<ChipFromFriends />]}
 					/>
 					<BotCard
 						label='PixelBot'
 						src='https://ts1.cn.mm.bing.net/th/id/R-C.0c8bf36e099654aadaf5f127ef1a3f1b?rik=uHrB%2blGez03%2fAA&riu=http%3a%2f%2fi3.img.969g.com%2fdown%2fimgx2014%2f10%2f24%2f289_102445_a1cff.jpg&ehk=EeF%2fioqRM6NfQqkCgXw%2bwLvO1%2fxZgeZ2pof7ALNLGsg%3d&risl=&pid=ImgRaw&r=0'
 						href='/device/robot/streaming/fullscreen'
-						bodyChips={[<ChipOffline />, <ChipCloudStorageExpired />]}
+						bodyChips={[<ChipOffline key={2} />, <ChipCloudStorageExpired key={3} />]}
 						footerChips={[<ChipShared />]}
 					/>
 				</div>
@@ -90,28 +82,28 @@ export function DeviceList() {
 						label='办公室1'
 						src='https://ts1.cn.mm.bing.net/th/id/R-C.4894a961ab87e3459babae4ef8a2f4fa?rik=1P7ZI7Evnz4Pqg&riu=http%3a%2f%2fpic.zsucai.com%2ffiles%2f2013%2f0830%2fxiaguang2.jpg&ehk=Ok%2fjrv35R0L218oT%2flliRL8DJc52pARVnWU%2bXOpUwq4%3d&risl=&pid=ImgRaw&r=0'
 						href='/device/streaming'
-						bodyChips={[<ChipOnline />, <ChipCloudStorageExpiring />]}
+						bodyChips={[<ChipOnline key={4} />, <ChipCloudStorageExpiring key={5} />]}
 						footerChips={[<ChipFromFriends />]}
 					/>
 					<IpcCard
 						label='办公室2'
 						src='https://ts1.cn.mm.bing.net/th/id/R-C.f54c83f04442cec528a250d251251ce6?rik=JE7BoZk5xK4iEg&riu=http%3a%2f%2fpic4.bbzhi.com%2ffengjingbizhi%2fgaoqingxifengjingzhuomianbizhixiazai%2fgaoqingxifengjingzhuomianbizhixiazai_366146_18.jpg&ehk=YvUnl11nBp%2fGJssQUbYqkXLo7fchkD%2fEQ8BGpW2Urjs%3d&risl=&pid=ImgRaw&r=0'
 						href='/device/streaming'
-						bodyChips={[<ChipOnline />, <ChipCloudStorageExpiring />]}
+						bodyChips={[<ChipOnline key={6} />, <ChipCloudStorageExpiring key={7} />]}
 						footerChips={[<ChipFromFriends />]}
 					/>
 					<IpcCard
 						label='办公室3'
 						src='https://ts1.cn.mm.bing.net/th/id/R-C.3edbd350d03c25ed988236c50d0733e6?rik=txi3%2f%2b%2fVYUJofg&riu=http%3a%2f%2fpic.zsucai.com%2ffiles%2f2013%2f0802%2fwmdqfj4.jpg&ehk=TY9%2f90VQn6m3NYCoiPX2UyRYQIT7dkGJtTJli1W7pfo%3d&risl=&pid=ImgRaw&r=0'
 						href='/device/streaming'
-						bodyChips={[<ChipOnline />, <ChipCloudStorageExpiring />]}
+						bodyChips={[<ChipOnline key={8} />, <ChipCloudStorageExpiring key={9} />]}
 						footerChips={[<ChipFromFriends />]}
 					/>
 					<IpcCard
 						label='办公室4'
 						src='https://ts1.cn.mm.bing.net/th/id/R-C.0c8bf36e099654aadaf5f127ef1a3f1b?rik=uHrB%2blGez03%2fAA&riu=http%3a%2f%2fi3.img.969g.com%2fdown%2fimgx2014%2f10%2f24%2f289_102445_a1cff.jpg&ehk=EeF%2fioqRM6NfQqkCgXw%2bwLvO1%2fxZgeZ2pof7ALNLGsg%3d&risl=&pid=ImgRaw&r=0'
 						href='/device/streaming'
-						bodyChips={[<ChipOnline />, <ChipCloudStorageExpiring />]}
+						bodyChips={[<ChipOnline key={10} />, <ChipCloudStorageExpiring key={11} />]}
 						footerChips={[<ChipFromFriends />]}
 					/>
 				</div>
--- app/[locale]/(nextui)/device2/page.jsx
+++ app/[locale]/(nextui)/device2/page.jsx
@@ -1,37 +1,27 @@
 'use client';
 
-import {
-	Button,
-	Badge,
-	Dropdown,
-	DropdownTrigger,
-	DropdownMenu,
-	DropdownItem,
-	Divider,
-} from '@nextui-org/react';
+import { Button, Badge, Divider } from '@nextui-org/react';
 import { NavTabs, ThemeButton } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { AppLogo } from '@/jujiu-ui-components/nextui/ipc/about/brand';
-import Link, { useJuJiuT } from '@/state/translate';
-import {
-	EnvelopeIcon,
-	PlusCircleIcon,
-	SquaresPlusIcon,
-	RectangleGroupIcon,
-	QrCodeIcon,
-} from '@heroicons/react/24/solid';
+import Link from '@/state/translate';
+import { EnvelopeIcon } from '@heroicons/react/24/solid';
+import { QrCodeScanner } from '@styled-icons/material-rounded/QrCodeScanner';
 import { DeviceList, DeviceMenu } from '../components';
 
 export default function Page() {
 	return (
 		<div className='h-screen flex flex-col'>
-			<div className='flex items-center justify-between p-2'>
+			<div className='flex items-center justify-between'>
 				<AppLogo />
-				<div className='flex items-centerpx-2'>
+				<div className='flex items-center'>
 					<ThemeButton />
 					<Button isIconOnly variant='light' as={Link} href='/message'>
 						<Badge content='5' color='primary'>
 							<EnvelopeIcon className='h-6 w-6' />
 						</Badge>
+					</Button>
+					<Button isIconOnly variant='light' as={Link} href='/device/scan'>
+						<QrCodeScanner size={24} />
 					</Button>
 					<DeviceMenu />
 				</div>

159b31c71dcea0d345568b26edc92ab3b3308df4
Jim Jin <jim_king_2000@163.com>
2023-11-14 17:53:29 +0800

    refactory

--- app/[locale]/nextui/robot/my/settings/page.jsx
+++ app/[locale]/nextui/robot/my/settings/page.jsx
@@ -1,34 +1,16 @@
 'use client';
 
-import { Button, User, Badge, Card, CardBody } from '@nextui-org/react';
 import { NavbarBack, LinkGroup } from '@/jujiu-ui-components/nextui/core/core-ui';
-import Link, { useJuJiuT } from '@/state/translate';
-import { EnvelopeIcon, Cog6ToothIcon } from '@heroicons/react/24/outline';
-import {
-	CalendarCheck,
-	VipCrown,
-	Medal,
-	Store,
-	Coupon,
-	FileText,
-	Profile,
-	ShoppingCart2,
-	CustomerService,
-	Mail,
-	Question,
-	ArrowUpCircle,
-	Stethoscope,
-	At,
-} from '@styled-icons/remix-line';
+import { useJuJiuT } from '@/state/translate';
 
 export default function Page() {
 	const t = useJuJiuT();
 
 	const linkData = [
 		{ label: t('账号设置'), href: '/my/account' },
-		{ label: t('通用设置'), icon: <VipCrown size={24} />, href: '/my/uisettings' },
-		{ label: t('消息设置'), icon: <Medal size={24} />, href: '/my/notification' },
-		{ label: t('关于'), icon: <Profile size={24} />, href: '/my/about', last: true },
+		{ label: t('通用设置'), href: '/my/uisettings' },
+		{ label: t('消息设置'), href: '/my/notification' },
+		{ label: t('关于'), href: '/my/about', last: true },
 	];
 
 	return (

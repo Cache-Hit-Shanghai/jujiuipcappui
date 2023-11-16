5142fb12c855a74163f44d7ee85d7f4a1a64bdad
jujiu <jujiu@outlook.com>
2023-11-16 10:49:27 +0800

    noti

--- app/[locale]/nextui/my/notification/page.jsx
+++ app/[locale]/nextui/my/notification/page.jsx
@@ -1,10 +1,15 @@
 'use client';
-import { NavbarBack } from '@/jujiu-ui-components/nextui/core/core-ui';
+import { NavbarBack, LabelLink } from '@/jujiu-ui-components/nextui/core/core-ui';
+import { ChangeNotification } from '@/jujiu-ui-components/nextui/ipc/settings/notification';
+
 const Page = () => {
 	return (
 		<div className='flex flex-col h-screen'>
 			<NavbarBack label={'消息通知设置 - 办3'}></NavbarBack>
-			<div className='flex flex-col shrink-0  overflow-auto px-4 gap-4'></div>
+			<div className='flex flex-col shrink-0  overflow-auto px-4 gap-4'>
+				<ChangeNotification></ChangeNotification>
+				<LabelLink label='AI报警事件类型' href='./type'></LabelLink>
+			</div>
 		</div>
 	);
 };

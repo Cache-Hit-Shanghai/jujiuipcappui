f8e12270493ea330faf5fb2d5bb67af252719353
Jim Jin <jim_king_2000@163.com>
2023-11-16 18:40:36 +0800

    refactory

--- app/[locale]/nextui/my/account/page.jsx
+++ app/[locale]/nextui/my/account/page.jsx
@@ -1,35 +1,35 @@
 'use client';
-import {
-	NavbarBack,
-	Information,
-	LinkButton,
-	LabelButton,
-	LabelLink,
-} from '@/jujiu-ui-components/nextui/core/core-ui';
 
 import {
 	Button,
 	Avatar,
-	Link,
+	Card,
+	CardBody,
 	Modal,
 	ModalContent,
-	ModalFooter,
+	Divider,
 	ModalBody,
 	ModalHeader,
 	Input,
 } from '@nextui-org/react';
-import { ChevronRightIcon, UserIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
+import { NavbarBack, Information, LabelButton, LabelLink } from '@/jujiu-ui-components/nextui/core/core-ui';
 
 function JuJiuModal({ onOpenChange, onClose, isOpen, title, position, children, ...props }) {
 	return (
-		<Modal isOpen={isOpen} placement={position || 'bottom'} onClose={onClose} onOpenChange={onOpenChange}>
+		<Modal
+			isOpen={isOpen}
+			placement={position || 'bottom'}
+			onClose={onClose}
+			onOpenChange={onOpenChange}
+			className='m-0'
+		>
 			<ModalContent>
 				{(onClose) => {
 					return (
 						<>
 							<ModalHeader>{title}</ModalHeader>
-							<ModalBody>{children}</ModalBody>
+							<ModalBody className='py-4'>{children}</ModalBody>
 							{/* <ModalFooter>
 								<Button color='danger' variant='light' onPress={onClose}>
 									取消
@@ -53,19 +53,25 @@ const Page = () => {
 	return (
 		<div className='flex flex-col h-screen'>
 			<NavbarBack label={'账号设置'}></NavbarBack>
-			<div className='flex flex-col shrink-0  overflow-auto px-4 gap-4'>
-				<LabelButton
-					label={'头像'}
-					onClick={() => {
-						setOpenAvata(true);
-					}}
-				>
-					<Avatar icon={<UserIcon width={'24px'} height={'24px'}></UserIcon>}></Avatar>
-				</LabelButton>
-				<LabelButton label={'昵称'} onClick={() => setOpenName(true)}>
-					用户12345
-				</LabelButton>
-				<LabelLink label={'修改密码'} href='./changePassword'></LabelLink>
+			<div className='flex flex-col px-4 gap-4'>
+				<Card>
+					<CardBody className='py-0'>
+						<LabelButton
+							label={'头像'}
+							onClick={() => {
+								setOpenAvata(true);
+							}}
+						>
+							<Avatar src='https://i.pravatar.cc/150?u=a04258114e29026702d' />
+						</LabelButton>
+						<Divider />
+						<LabelButton label={'昵称'} onClick={() => setOpenName(true)}>
+							用户12345
+						</LabelButton>
+						<Divider />
+						<LabelLink label={'修改密码'} href='./changePassword'></LabelLink>
+					</CardBody>
+				</Card>
 				<Button color='warning' variant='bordered'>
 					退出登录
 				</Button>
@@ -76,7 +82,6 @@ const Page = () => {
 			<JuJiuModal isOpen={openAvata} onOpenChange={setOpenAvata} title={'更换头像'}>
 				<Button color='success'>本地相册</Button>
 			</JuJiuModal>
-
 			<JuJiuModal isOpen={openName} onOpenChange={setOpenName} title={'修改昵称'}>
 				<Input placeholder={'请填写昵称……'} maxLength={20} minLength={2} />
 				<Information label={'修改昵称提示'} />

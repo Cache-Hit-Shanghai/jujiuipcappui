4f7a28032132365bcff26fcaa1559ca6082d6e67
Jim Jin <jim_king_2000@163.com>
2023-11-16 19:13:42 +0800

    refactory

--- app/[locale]/nextui/my/notification/page.jsx
+++ app/[locale]/nextui/my/notification/page.jsx
@@ -1,4 +1,6 @@
 'use client';
+
+import { Card, CardBody } from '@nextui-org/react';
 import { NavbarBack, LabelLink } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { ChangeNotification } from '@/jujiu-ui-components/nextui/ipc/settings/notification';
 
@@ -6,9 +8,17 @@ const Page = () => {
 	return (
 		<div className='flex flex-col h-screen'>
 			<NavbarBack label={'消息通知设置 - 办3'}></NavbarBack>
-			<div className='flex flex-col shrink-0  overflow-auto px-4 gap-4'>
-				<ChangeNotification></ChangeNotification>
-				<LabelLink label='AI报警事件类型' href='./type'></LabelLink>
+			<div className='flex flex-col shrink-0 overflow-auto px-4 gap-4'>
+				<Card>
+					<CardBody>
+						<ChangeNotification />
+					</CardBody>
+				</Card>
+				<Card>
+					<CardBody>
+						<LabelLink label='AI报警事件类型' href='./type' />
+					</CardBody>
+				</Card>
 			</div>
 		</div>
 	);

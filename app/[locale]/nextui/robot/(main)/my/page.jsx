50ecebb11661cbaf6b28747979de017b04d85593
Jim Jin <jim_king_2000@163.com>
2023-11-17 17:41:27 +0800

    refactory

--- app/[locale]/nextui/robot/(main)/my/page.jsx
+++ app/[locale]/nextui/robot/(main)/my/page.jsx
@@ -1,66 +1,36 @@
 'use client';
 
-import { Button, User, Badge, Card, CardBody, Avatar } from '@nextui-org/react';
-import { ThemeButton } from '@/jujiu-ui-components/nextui/core/core-ui';
+import { Button, Image, Badge, Card, CardBody, Avatar } from '@nextui-org/react';
+import { ThemeButton, LinkGroup } from '@/jujiu-ui-components/nextui/core/core-ui';
 import Link, { useJuJiuT } from '@/state/translate';
 import { Envelope } from '@styled-icons/heroicons-outline/Envelope';
 import { Cog6Tooth } from '@styled-icons/heroicons-outline/Cog6Tooth';
-import { CalendarCheck } from '@styled-icons/remix-line/CalendarCheck';
-import { VipCrown } from '@styled-icons/remix-line/VipCrown';
+import { Suitcase } from '@styled-icons/remix-line/Suitcase';
+import { MapPin } from '@styled-icons/remix-line/MapPin';
 import { Medal } from '@styled-icons/remix-line/Medal';
-import { Store } from '@styled-icons/remix-line/Store';
-import { Coupon } from '@styled-icons/remix-line/Coupon';
-import { FileText } from '@styled-icons/remix-line/FileText';
-import { Stethoscope } from '@styled-icons/remix-line/Stethoscope';
-import { Profile } from '@styled-icons/remix-line/Profile';
+import { Share } from '@styled-icons/remix-line/Share';
 import { ShoppingCart2 } from '@styled-icons/remix-line/ShoppingCart2';
 import { CustomerService } from '@styled-icons/remix-line/CustomerService';
-import { Mail } from '@styled-icons/remix-line/Mail';
 import { Question } from '@styled-icons/remix-line/Question';
 import { ArrowUpCircle } from '@styled-icons/remix-line/ArrowUpCircle';
-import { At } from '@styled-icons/remix-line/At';
-
-function StatisticCard({ data }) {
-	return (
-		<div className='flex flex-row gap-4'>
-			{data.map((datum) => (
-				<div key={datum.label} className='flex flex-row items-baseline'>
-					<p className='px-1 font-bold'>{datum.number}</p>
-					<p className='text-sm text-default-400'>{datum.label}</p>
-				</div>
-			))}
-		</div>
-	);
-}
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Page() {
 	const t = useJuJiuT();
 
-	const statistics = [
-		{ label: '关注', number: 0 },
-		{ label: '粉丝', number: 0 },
-		{ label: '好友', number: 0 },
-		{ label: '皮豆', number: 0 },
-	];
-
 	const linkData0 = [
-		{ label: '签到', icon: <CalendarCheck size={24} />, href: '' },
-		{ label: '会员', icon: <VipCrown size={24} />, href: '' },
-		{ label: '勋章', icon: <Medal size={24} />, href: '' },
-		{ label: '宠物信息', icon: <Profile size={24} />, href: '' },
-		{ label: '商城', icon: <Store size={24} />, href: '' },
-		{ label: '订单', icon: <ShoppingCart2 size={24} />, href: '' },
-		{ label: '优惠券', icon: <Coupon size={24} />, href: '' },
-		{ label: '会员权益', icon: <FileText size={24} />, href: '' },
+		{ label: '我的收藏', icon: <Suitcase size={24} />, href: '' },
+		{ label: '我的订单', icon: <ShoppingCart2 size={24} />, href: '' },
+		{ label: '退换售后', icon: <Medal size={24} />, href: '' },
+		{ label: '收货地址', icon: <MapPin size={24} />, href: '', last: true },
 	];
 
 	const linkData1 = [
-		{ label: '宠物医生', icon: <Stethoscope size={24} />, href: '' },
-		{ label: '客服', icon: <CustomerService size={24} />, href: '' },
-		{ label: '投诉与反馈', icon: <Mail size={24} />, href: '' },
+		{ label: '设备分享', icon: <Share size={24} />, href: '' },
 		{ label: '固件升级', icon: <ArrowUpCircle size={24} />, href: '' },
-		{ label: '帮助', icon: <Question size={24} />, href: '' },
-		{ label: '关于小皮', icon: <At size={24} />, href: '' },
+		{ label: '我的客服', icon: <CustomerService size={24} />, href: '' },
+		{ label: '帮助与反馈', icon: <Question size={24} />, href: '', last: true },
 	];
 
 	return (
@@ -80,12 +50,26 @@ export default function Page() {
 					</div>
 				</div>
 			</div>
-			<div className='flex flex-col items-center gap-2'>
+			<div className='flex flex-col items-center pb-4 gap-2 overflow-auto'>
 				<Avatar src='https://i.pravatar.cc/150?u=a04258114e29026708c' className='w-20 h-20 text-large' />
 				<p>暴走的鸡腿</p>
 				<p>关注: 1 粉丝: 0 好友: 4</p>
 			</div>
-			<div className='flex flex-col flex-1 px-4 gap-4'></div>
+			<div className='flex flex-col flex-1 px-4 gap-4'>
+				<LinkGroup data={linkData0} />
+				<Carousel
+					autoPlay
+					infiniteLoop
+					dynamicHeight
+					showArrows={false}
+					showStatus={false}
+					showThumbs={false}
+				>
+					<Image src='https://pixelbot.oss-accelerate.aliyuncs.com/my0.png' />
+					<Image src='https://pixelbot.oss-accelerate.aliyuncs.com/my1.png' />
+				</Carousel>
+				<LinkGroup data={linkData1} />
+			</div>
 		</>
 	);
 }

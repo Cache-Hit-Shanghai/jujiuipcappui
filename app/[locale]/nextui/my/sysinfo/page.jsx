4bfb8dd0b3defb87cf9368e3ba5a1142e057f28c
Jim Jin <jim_king_2000@163.com>
2023-11-17 10:39:57 +0800

    refactory

--- app/[locale]/nextui/my/sysinfo/page.jsx
+++ app/[locale]/nextui/my/sysinfo/page.jsx
@@ -1,23 +1,26 @@
 'use client';
-import { NavbarBack, InfoGroup } from '@/jujiu-ui-components/nextui/core/core-ui';
 
-const infos = [
-	{ key: '应用版本', value: '2023/7/16' },
-	{ key: 'App/小程序版本', value: '1.0.0.2' },
-	{ key: '操作系统', value: 'Android 10' },
-	{ key: 'WebView', value: '114.0.5735.61' },
-	{
-		key: '用户代理',
-		value:
-			'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
-	},
-	{ key: '注册ID', value: '' },
-];
+import { NavbarBack, InfoGroup } from '@/jujiu-ui-components/nextui/core/core-ui';
+import { useJuJiuT } from '@/state/translate';
 
 const Page = () => {
+	const t = useJuJiuT();
+	const infos = [
+		{ key: t('应用版本'), value: '2023/7/16' },
+		{ key: t('App/小程序版本'), value: '1.0.0.2' },
+		{ key: t('操作系统'), value: 'Android 10' },
+		{ key: 'WebView', value: '114.0.5735.61' },
+		{ key: t('注册ID'), value: 'abcdefghijklmn' },
+		{
+			key: t('用户代理'),
+			value:
+				'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+		},
+	];
+
 	return (
 		<div className='flex flex-col h-screen'>
-			<NavbarBack label={'系统信息'}></NavbarBack>
+			<NavbarBack label={t('系统信息')}></NavbarBack>
 			<div className='flex flex-col grow shrink-0  overflow-auto px-4 gap-4'>
 				<InfoGroup data={infos}></InfoGroup>
 			</div>
--- app/[locale]/nextui/my/uisettings/page.jsx
+++ app/[locale]/nextui/my/uisettings/page.jsx
@@ -1,11 +1,14 @@
 'use client';
+
 import { NavbarBack } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { ChangeUI } from '@/jujiu-ui-components/nextui/ipc/settings/ui';
+import { useJuJiuT } from '@/state/translate';
 
 const Page = () => {
+	const t = useJuJiuT();
 	return (
 		<div className='flex flex-col h-screen'>
-			<NavbarBack label={'界面设置'}></NavbarBack>
+			<NavbarBack label={t('界面设置')}></NavbarBack>
 			<div className='flex flex-col shrink-0  overflow-auto px-4 gap-4'>
 				<ChangeUI></ChangeUI>
 			</div>

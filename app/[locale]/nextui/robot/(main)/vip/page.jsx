3c85b1c7cb48fa13a7902b34a4bdb7ae51380793
Jim Jin <jim_king_2000@163.com>
2023-11-17 16:33:10 +0800

    refactory

--- app/[locale]/nextui/robot/(main)/vip/page.jsx
+++ app/[locale]/nextui/robot/(main)/vip/page.jsx
@@ -1,67 +1,16 @@
 'use client';
 
-import { Button, User, Badge, Card, CardHeader, CardBody, Chip } from '@nextui-org/react';
-import { ThemeButton } from '@/jujiu-ui-components/nextui/core/core-ui';
-import Link, { useJuJiuT } from '@/state/translate';
-import { Envelope } from '@styled-icons/heroicons-outline/Envelope';
-import { Cog6Tooth } from '@styled-icons/heroicons-outline/Cog6Tooth';
+import { Button, Spacer, Image, Card, CardHeader, CardBody, Chip } from '@nextui-org/react';
+import { useJuJiuT } from '@/state/translate';
 import { CalendarCheck } from '@styled-icons/remix-line/CalendarCheck';
-import { VipCrown } from '@styled-icons/remix-line/VipCrown';
-import { Medal } from '@styled-icons/remix-line/Medal';
 import { Store } from '@styled-icons/remix-line/Store';
 import { Coupon } from '@styled-icons/remix-line/Coupon';
-import { FileText } from '@styled-icons/remix-line/FileText';
-import { Stethoscope } from '@styled-icons/remix-line/Stethoscope';
-import { Profile } from '@styled-icons/remix-line/Profile';
-import { ShoppingCart2 } from '@styled-icons/remix-line/ShoppingCart2';
-import { CustomerService } from '@styled-icons/remix-line/CustomerService';
-import { Mail } from '@styled-icons/remix-line/Mail';
-import { Question } from '@styled-icons/remix-line/Question';
-import { ArrowUpCircle } from '@styled-icons/remix-line/ArrowUpCircle';
-import { At } from '@styled-icons/remix-line/At';
-
-function StatisticCard({ data }) {
-	return (
-		<div className='flex flex-row gap-4'>
-			{data.map((datum) => (
-				<div key={datum.label} className='flex flex-row items-baseline'>
-					<p className='px-1 font-bold'>{datum.number}</p>
-					<p className='text-sm text-default-400'>{datum.label}</p>
-				</div>
-			))}
-		</div>
-	);
-}
+import { VipDiamond } from '@styled-icons/remix-line/VipDiamond';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Page() {
 	const t = useJuJiuT();
-
-	const statistics = [
-		{ label: '关注', number: 0 },
-		{ label: '粉丝', number: 0 },
-		{ label: '好友', number: 0 },
-		{ label: '皮豆', number: 0 },
-	];
-
-	const linkData0 = [
-		{ label: '签到', icon: <CalendarCheck size={24} />, href: '' },
-		{ label: '会员', icon: <VipCrown size={24} />, href: '' },
-		{ label: '勋章', icon: <Medal size={24} />, href: '' },
-		{ label: '宠物信息', icon: <Profile size={24} />, href: '' },
-		{ label: '商城', icon: <Store size={24} />, href: '' },
-		{ label: '订单', icon: <ShoppingCart2 size={24} />, href: '' },
-		{ label: '优惠券', icon: <Coupon size={24} />, href: '' },
-		{ label: '会员权益', icon: <FileText size={24} />, href: '' },
-	];
-
-	const linkData1 = [
-		{ label: '宠物医生', icon: <Stethoscope size={24} />, href: '' },
-		{ label: '客服', icon: <CustomerService size={24} />, href: '' },
-		{ label: '投诉与反馈', icon: <Mail size={24} />, href: '' },
-		{ label: '固件升级', icon: <ArrowUpCircle size={24} />, href: '' },
-		{ label: '帮助', icon: <Question size={24} />, href: '' },
-		{ label: '关于小皮', icon: <At size={24} />, href: '' },
-	];
 
 	return (
 		<>
@@ -86,6 +35,81 @@ export default function Page() {
 						<Button variant='bordered'>智能识别</Button>
 						<Button variant='bordered'>电话提醒</Button>
 						<Button variant='bordered'>异常巡检</Button>
+					</CardBody>
+				</Card>
+				<Card className='grid grid-cols-2 gap-0.5'>
+					<div className='flex flex-col h-16'>
+						<Button radius='none' variant='flat' className='h-full'>
+							<div className='flex flex-row items-center w-full'>
+								<div>
+									<CalendarCheck size={36} />
+								</div>
+								<Spacer x={4} />
+								<div className='flex flex-col items-start'>
+									<div className='text-lg'>签到</div>
+									<div className='text-sm text-default-400'>连续签到得积分</div>
+								</div>
+							</div>
+						</Button>
+					</div>
+					<div className='flex flex-col h-16'>
+						<Button radius='none' variant='flat' className='h-full'>
+							<div className='flex flex-row items-center w-full'>
+								<div>
+									<Coupon size={36} />
+								</div>
+								<Spacer x={4} />
+								<div className='flex flex-col items-start'>
+									<div className='text-lg'>优惠券</div>
+									<div className='text-sm text-default-400'>0张</div>
+								</div>
+							</div>
+						</Button>
+					</div>
+					<div className='flex flex-col h-16'>
+						<Button radius='none' variant='flat' className='h-full'>
+							<div className='flex flex-row items-center w-full'>
+								<div>
+									<VipDiamond size={36} />
+								</div>
+								<Spacer x={4} />
+								<div className='flex flex-col items-start'>
+									<div className='text-lg'>我的皮豆</div>
+									<div className='text-sm text-default-400'>0</div>
+								</div>
+							</div>
+						</Button>
+					</div>
+					<div className='flex flex-col h-16'>
+						<Button radius='none' variant='flat' className='h-full'>
+							<div className='flex flex-row items-center w-full'>
+								<div className='text-lg'>
+									<Store size={36} />
+								</div>
+								<Spacer x={4} />
+								<div className='flex flex-col items-start'>
+									<div className='text-lg'>皮豆商城</div>
+									<div className='text-sm text-default-400'>实用好物</div>
+								</div>
+							</div>
+						</Button>
+					</div>
+				</Card>
+				<Card>
+					<CardHeader>会员专属福利</CardHeader>
+					<CardBody className='p-0'>
+						<Carousel
+							autoPlay
+							infiniteLoop
+							dynamicHeight
+							showArrows={false}
+							showStatus={false}
+							showThumbs={false}
+						>
+							<Image src='https://pixelbot.oss-accelerate.aliyuncs.com/vip0.png' />
+							<Image src='https://pixelbot.oss-accelerate.aliyuncs.com/vip1.png' />
+							<Image src='https://pixelbot.oss-accelerate.aliyuncs.com/vip2.png' />
+						</Carousel>
 					</CardBody>
 				</Card>
 			</div>

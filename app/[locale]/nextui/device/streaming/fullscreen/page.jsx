77a074f9ecbd5325deacca17477493c3e03465fb
Jim <jim_king_2000@163.com>
2023-11-20 21:33:47 +0800

    add exit animation for /streaming/fullscreen

--- app/[locale]/nextui/device/streaming/fullscreen/page.jsx
+++ app/[locale]/nextui/device/streaming/fullscreen/page.jsx
@@ -1,18 +1,11 @@
 'use client';
 
-import { Button, Card, Tabs, Tab, Spinner } from '@nextui-org/react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 import { useIdleTimer } from 'react-idle-timer';
-import { Moon } from '@styled-icons/heroicons-outline/Moon';
-import { ArrowTopRightOnSquare } from '@styled-icons/heroicons-outline/ArrowTopRightOnSquare';
-import { Cog6Tooth } from '@styled-icons/heroicons-outline/Cog6Tooth';
-import { ExclamationTriangle } from '@styled-icons/heroicons-outline/ExclamationTriangle';
-import { NavbarBack, LinkButton } from '@/jujiu-ui-components/nextui/core/core-ui';
-import { ChipRecording } from '@/jujiu-ui-components/nextui/core/core-chips';
+import { NavbarBack } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { PanControl } from '@/jujiu-ui-components/nextui/ipc/video/panControl';
 import { ScreenCopyControl, ChatControl } from '@/jujiu-ui-components/nextui/ipc/video/toolControl';
-import { Orientation } from '@styled-icons/fluentui-system-filled/Orientation';
 import { useJuJiuT } from '@/state/translate';
 
 export default function Page() {
@@ -35,28 +28,32 @@ export default function Page() {
 					/>
 				</video>
 			</div>
-			<motion.div
-				className={show ? 'visible' : 'collapse'}
-				initial={{ opacity: 0 }}
-				animate={show ? { opacity: 1 } : { opacity: 0 }}
-				transition={{ ease: 'linear', duration: 0.5 }}
-			>
-				<NavbarBack
-					className='absolute'
-					label={t('实时视频') + ' - 办3'}
-					onPress={() => {
-						screen.orientation.unlock();
-						document.exitFullscreen();
-					}}
-				/>
-				<div className='absolute left-0 inset-y-0 m-auto p-2 h-fit'>
-					<PanControl />
-				</div>
-				<div className='absolute bottom-0 inset-x-0 m-auto p-2 w-fit flex flex-row gap-4'>
-					<ScreenCopyControl />
-					<ChatControl speaking={speaking} onPress={() => setSpeaking(!speaking)} />
-				</div>
-			</motion.div>
+			<AnimatePresence>
+				{show && (
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ ease: 'linear', duration: 0.5 }}
+					>
+						<NavbarBack
+							className='absolute'
+							label={t('实时视频') + ' - 办3'}
+							onPress={() => {
+								screen.orientation.unlock();
+								document.exitFullscreen();
+							}}
+						/>
+						<div className='absolute left-0 inset-y-0 m-auto p-2 h-fit'>
+							<PanControl />
+						</div>
+						<div className='absolute bottom-0 inset-x-0 m-auto p-2 w-fit flex flex-row gap-4'>
+							<ScreenCopyControl />
+							<ChatControl speaking={speaking} onPress={() => setSpeaking(!speaking)} />
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</div>
 	);
 }

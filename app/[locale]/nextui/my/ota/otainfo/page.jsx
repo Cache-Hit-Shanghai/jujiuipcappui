aa325b35ef075dae5fded6b9bb4473ba11b67dfa
Jim <jim_king_2000@163.com>
2023-11-27 21:31:42 +0800

    fix build break

--- app/[locale]/nextui/my/ota/otainfo/page.jsx
+++ app/[locale]/nextui/my/ota/otainfo/page.jsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { Card, CardBody } from '@nextui-org/react';
+import { Card, CardHeader, CardBody, CardFooter, Divider } from '@nextui-org/react';
 import { NavbarBack, LinkButton, MobileHeader, MobileMain } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { Update } from '@styled-icons/material/Update';
+import { NestCamWiredStand } from '@styled-icons/material/NestCamWiredStand';
 
 const Page = () => {
 	return (
@@ -12,23 +13,30 @@ const Page = () => {
 			</MobileHeader>
 			<MobileMain>
 				<Card>
-					<CardBody className='flex flex-col gap-2'>
-						<div className='flex items-center justify-center gap-2'>
-							<Update size={48} />
-							<div className='flex flex-col items-center'>
-								<p>最新版本：15.2.3.4</p>
-								<p>当前版本：15.2.3.0</p>
-							</div>
+					<CardHeader className='flex items-center justify-center gap-2'>
+						<NestCamWiredStand size={48} />
+						<div className='flex flex-col items-center'>
+							<p>最新版本：15.2.3.4</p>
+							<p>当前版本：15.2.3.0</p>
 						</div>
-						<div>
-							<h2 className='font-semibold text-2xl'>更新日志：</h2>
-							<p>1. 修复已知问题若干。</p>
-							<p>2. 新增拉流时断线重连的功能。</p>
-						</div>
-						<div className='flex justify-center'>
-							<LinkButton size='sm' variant='solid' color='primary' href='./updating' label='现在升级' />
-						</div>
+					</CardHeader>
+					<Divider />
+					<CardBody>
+						<h2 className='font-semibold text-2xl'>更新日志：</h2>
+						<p>1. 修复已知问题若干。</p>
+						<p>2. 新增拉流时断线重连的功能。</p>
 					</CardBody>
+					<Divider />
+					<CardFooter className='justify-center'>
+						<LinkButton
+							size='sm'
+							variant='solid'
+							color='primary'
+							href='./updating'
+							label='现在升级'
+							startContent={<Update size={24} />}
+						/>
+					</CardFooter>
 				</Card>
 			</MobileMain>
 		</div>
--- app/[locale]/nextui/my/ota/page.jsx
+++ app/[locale]/nextui/my/ota/page.jsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { NavbarBack, MobileHeader, MobileMain } from '@/jujiu-ui-components/nextui/core/core-ui';
-import { OtaDeviceCard } from '@/jujiu-ui-components/nextui/ipc/ota/card';
+import { Card, CardBody } from '@nextui-org/react';
+import { NavbarBack, LinkButton, MobileHeader, MobileMain } from '@/jujiu-ui-components/nextui/core/core-ui';
+import { NestCamWiredStand } from '@styled-icons/material/NestCamWiredStand';
 
 const data = [
 	{ device: '客厅', size: '150MB', from: '1.0.0', to: '1.1.0' },
@@ -17,13 +18,31 @@ const Page = () => {
 			</MobileHeader>
 			<MobileMain>
 				{data.map(({ device, size, from, to }, i) => (
-					<OtaDeviceCard
-						key={i}
-						device={{ _id: '1', desc: device }}
-						currentVersion={from}
-						upgradeVersion={to}
-						upgradeSize={size}
-					></OtaDeviceCard>
+					<Card key={i}>
+						<CardBody>
+							<div className='flex justify-between items-center gap-2'>
+								<div className='flex  items-center gap-2'>
+									<NestCamWiredStand size={48} />
+									<div>
+										<p>{device}</p>
+										<p className='text-xs text-default-500'>{size}</p>
+										<p className='text-xs text-default-500'>
+											{from} ~ {to}
+										</p>
+									</div>
+								</div>
+								<div>
+									<LinkButton
+										size='sm'
+										variant='solid'
+										color='primary'
+										href='./otainfo'
+										label='现在升级'
+									></LinkButton>
+								</div>
+							</div>
+						</CardBody>
+					</Card>
 				))}
 			</MobileMain>
 		</div>

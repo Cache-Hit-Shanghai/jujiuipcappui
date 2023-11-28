d2f5bea83bb34da64c1662b3b99074482c5efdc0
Jim Jin <jim_king_2000@163.com>
2023-11-28 17:47:39 +0800

    refactory

--- app/[locale]/nextui/my/other/page.jsx
+++ app/[locale]/nextui/my/other/page.jsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { Button } from '@nextui-org/react';
+import { Button, Card, CardBody, Divider, useDisclosure } from '@nextui-org/react';
 import { useState } from 'react';
 import { Check } from '@styled-icons/material/Check';
 import { NavbarBack, MobileHeader, MobileMain } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { Toast } from '@/jujiu-ui-components/nextui/extension/toast';
+import { Confirm } from '@/jujiu-ui-components/nextui/extension/confirm';
 
 const Page = () => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [isOpenToast, setIsOpenToast] = useState(false);
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	return (
 		<div className='flex flex-col h-screen'>
@@ -15,12 +17,20 @@ const Page = () => {
 				<NavbarBack label='其它(生产环境无此页)' />
 			</MobileHeader>
 			<MobileMain>
-				<Button size='sm' onPress={() => setIsOpen(true)}>
-					Toast
-				</Button>
+				<Card>
+					<CardBody className='gap-2'>
+						<Button size='sm' onPress={() => setIsOpenToast(true)}>
+							Toast
+						</Button>
+						<Divider />
+						<Button size='sm' onPress={onOpen}>
+							dialog
+						</Button>
+					</CardBody>
+				</Card>
 				<Toast
-					isOpen={isOpen}
-					onClose={() => setIsOpen(false)}
+					isOpen={isOpenToast}
+					onClose={() => setIsOpenToast(false)}
 					message='This operation completes successfully. This toast will close in 5 seconds.'
 					startContent={
 						<div className='rounded-full bg-success p-1 flex'>
@@ -28,6 +38,7 @@ const Page = () => {
 						</div>
 					}
 				/>
+				<Confirm title='Confirmation' message='Do you want to exit?' isOpen={isOpen} onClose={onClose} />
 			</MobileMain>
 		</div>
 	);

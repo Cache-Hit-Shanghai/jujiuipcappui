75125ab3731e61a1d46b6e69370a1e9beac78ffb
Jim Jin <jim_king_2000@163.com>
2023-12-01 12:30:52 +0800

    refactory

--- app/[locale]/(nextui)/(main)/vip/page.jsx
+++ app/[locale]/(nextui)/(main)/vip/page.jsx
@@ -1,24 +1,18 @@
 'use client';
 
-import { Button, Divider, Card, CardHeader, CardBody } from '@nextui-org/react';
-import {
-	LinkButton,
-	LabelLink,
-	LinkGroup,
-	ThemeButton,
-	MobileHeader,
-	MobileMain,
-} from '@/jujiu-ui-components/nextui/core/core-ui';
-import Link, { useJuJiuT } from '@/state/translate';
-import { PhotoAlbum } from '@styled-icons/material/PhotoAlbum';
+import { Divider, Card, CardHeader, CardBody } from '@nextui-org/react';
+import { LinkButton, MobileHeader, MobileMain } from '@/jujiu-ui-components/nextui/core/core-ui';
+import { useJuJiuT } from '@/state/translate';
 import { CloudUpload } from '@styled-icons/material/CloudUpload';
-import { Update } from '@styled-icons/material/Update';
-import { IosShare } from '@styled-icons/material/IosShare';
-import { HelpOutline } from '@styled-icons/material/HelpOutline';
 import { Call } from '@styled-icons/material/Call';
 import { Mic } from '@styled-icons/material/Mic';
 import { SdStorage } from '@styled-icons/material/SdStorage';
 import { PhotoLibrary } from '@styled-icons/material/PhotoLibrary';
+import { Hub } from '@styled-icons/material/Hub';
+import { GraphicEq } from '@styled-icons/material/GraphicEq';
+import { Elderly } from '@styled-icons/material/Elderly';
+import { Storefront } from '@styled-icons/material/Storefront';
+import { ChairAlt } from '@styled-icons/material/ChairAlt';
 import { ComputerVision } from '@/jujiu-ui-components/icons/computervision';
 import { FrameInspect } from '@/jujiu-ui-components/icons/frameinspect';
 
@@ -26,67 +20,118 @@ export default function Page() {
 	const t = useJuJiuT();
 
 	return (
-		<MobileMain>
-			<Card>
-				<CardHeader>{t('通用服务')}</CardHeader>
-				<Divider />
-				<CardBody>
-					<div className='grid grid-cols-3 gap-y-2'>
-						<LinkButton
-							className='p-2 min-w-0'
-							radius='none'
-							label={t('云存储')}
-							icon={<CloudUpload size={24} />}
-							href='../my/cloudstorage'
-						/>
-						<LinkButton
-							className='p-2 min-w-0'
-							radius='none'
-							label={t('电话提醒')}
-							icon={<Call size={24} />}
-							href=''
-						/>
-						<LinkButton
-							className='p-2 min-w-0'
-							radius='none'
-							label={t('智能识别')}
-							icon={<ComputerVision size={24} />}
-							href=''
-						/>
-						<LinkButton
-							className='p-2 min-w-0'
-							radius='none'
-							label={t('语音助手')}
-							icon={<Mic size={24} />}
-							href=''
-						/>
-						<LinkButton
-							className='p-2 min-w-0'
-							radius='none'
-							label={t('画面巡检')}
-							icon={<FrameInspect size={24} />}
-							href=''
-						/>
-						<LinkButton
-							className='p-2 min-w-0'
-							radius='none'
-							label={t('时光相册')}
-							icon={<PhotoLibrary size={24} />}
-							href=''
-						/>
-						<LinkButton
-							className='p-2 min-w-0'
-							radius='none'
-							label={t('物联4G')}
-							icon={<SdStorage size={24} />}
-							href=''
-						/>
-					</div>
-				</CardBody>
-			</Card>
-			<Card>
-				<CardBody></CardBody>
-			</Card>
-		</MobileMain>
+		<>
+			<MobileHeader className='justify-center'>
+				<h4 className='h-10 flex items-center justify-center text-lg'>{t('会员服务')}</h4>
+			</MobileHeader>
+			<MobileMain>
+				<Card>
+					<CardHeader>{t('通用服务')}</CardHeader>
+					<Divider />
+					<CardBody>
+						<div className='grid grid-cols-3 gap-y-2'>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('云存储')}
+								icon={<CloudUpload size={24} />}
+								href='../my/cloudstorage'
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('电话提醒')}
+								icon={<Call size={24} />}
+								href=''
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('智能识别')}
+								icon={<ComputerVision size={24} />}
+								href=''
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('语音助手')}
+								icon={<Mic size={24} />}
+								href=''
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('画面巡检')}
+								icon={<FrameInspect size={24} />}
+								href=''
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('时光相册')}
+								icon={<PhotoLibrary size={24} />}
+								href=''
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('物联4G')}
+								icon={<SdStorage size={24} />}
+								href=''
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('分享流量套餐')}
+								icon={<Hub size={24} />}
+								href=''
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('语音备忘')}
+								icon={<GraphicEq size={24} />}
+								href=''
+							/>
+						</div>
+					</CardBody>
+				</Card>
+				<Card>
+					<CardHeader>{t('生活服务')}</CardHeader>
+					<CardBody>
+						<div className='grid grid-cols-3 gap-y-2'>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('老人看护')}
+								icon={<Elderly size={24} />}
+								href=''
+							/>
+						</div>
+					</CardBody>
+				</Card>
+				<Card>
+					<CardHeader>{t('店铺工具')}</CardHeader>
+					<CardBody>
+						<div className='grid grid-cols-3 gap-y-2'>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('在线值守')}
+								icon={<Storefront size={24} />}
+								href=''
+							/>
+							<LinkButton
+								className='p-2 min-w-0'
+								radius='none'
+								label={t('离岗检测')}
+								icon={<ChairAlt size={24} />}
+								href=''
+							/>
+						</div>
+					</CardBody>
+				</Card>
+			</MobileMain>
+		</>
 	);
 }

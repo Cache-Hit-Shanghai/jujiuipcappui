3e26bbe2a9f92f6fa22d90c11264c27ad504d84c
Jim Jin <jim_king_2000@163.com>
2023-12-01 14:20:08 +0800

    refactory

--- app/[locale]/(nextui)/device/binding/page.jsx
+++ app/[locale]/(nextui)/device/binding/page.jsx
@@ -1,17 +1,27 @@
 'use client';
 
-import { Input } from '@nextui-org/react';
+import { Input, Autocomplete, AutocompleteItem } from '@nextui-org/react';
 import { LinkButton } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { Info } from '@styled-icons/material/Info';
+import { Wifi } from '@styled-icons/material/Wifi';
+import { Lock } from '@styled-icons/material/Lock';
 import { useJuJiuT } from '@/state/translate';
+
+const WiFiList = ['A90_5G', 'A90_5G_WPA', 'DX-OFFICE', 'ChinaNet-0058', 'ChinaNet-iA4K', '章璐的iPhone'];
 
 export default function Page() {
 	const t = useJuJiuT();
 
 	return (
 		<>
-			<Input size='sm' label={t('WiFi名称(SSID)')} />
-			<Input size='sm' label={t('密码')} type='password' />
+			<Autocomplete size='sm' label={t('WiFi名称(SSID)')} startContent={<Wifi size={20} />}>
+				{WiFiList.map((ssid) => (
+					<AutocompleteItem key={ssid} value={ssid} startContent={<Wifi size={24} />}>
+						{ssid}
+					</AutocompleteItem>
+				))}
+			</Autocomplete>
+			<Input size='sm' label={t('密码')} type='password' startContent={<Lock size={20} />} />
 			<div className='flex flex-row gap-2 items-center text-warning text-sm'>
 				<Info size={24} className='flex-none' />
 				{t('请输入需要网络摄像机连接的WiFi名称和密码，随后点击“下一步”。')}

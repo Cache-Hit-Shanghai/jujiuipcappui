09290d31db5c35774982ab7e8dbeaec11229e4b4
Jim Jin <jim_king_2000@163.com>
2023-12-04 16:42:40 +0800

    refactory

--- app/[locale]/(nextui)/my/other/page.jsx
+++ app/[locale]/(nextui)/my/other/page.jsx
@@ -1,18 +1,15 @@
 'use client';
 
 import { Button, Card, CardBody, Divider, useDisclosure } from '@nextui-org/react';
-import { useState } from 'react';
 import { Check } from '@styled-icons/material/Check';
 import { Close } from '@styled-icons/material/Close';
-import { Exclamation } from '@/jujiu-ui-components/icons/exclamation';
+import { Warning } from '@styled-icons/material/Warning';
+import { InfoI } from '@/jujiu-ui-components/icons/infoi';
 import { NavbarBack, MobileHeader, MobileMain } from '@/jujiu-ui-components/nextui/core/core-ui';
-import { Toast } from '@/jujiu-ui-components/nextui/extension/toast';
+import { ToastProvider } from '@/jujiu-ui-components/nextui/extension/toast2';
 import { Confirm } from '@/jujiu-ui-components/nextui/extension/confirm';
 
 const Page = () => {
-	const [isOpenToastSuccess, setIsOpenToastSuccess] = useState(false);
-	const [isOpenToastError, setIsOpenToastError] = useState(false);
-	const [isOpenToastWarning, setIsOpenToastWarning] = useState(false);
 	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	return (
@@ -21,55 +18,95 @@ const Page = () => {
 				<NavbarBack label='其它(生产环境无此页)' />
 			</MobileHeader>
 			<MobileMain>
+				<ToastProvider>
+					{(state) => (
+						<Card>
+							<CardBody className='gap-2'>
+								<Button
+									onPress={() =>
+										state.add(
+											<div className='flex flex-row items-center gap-2'>
+												<div className='rounded-full bg-success p-1 flex'>
+													<Check size={20} />
+												</div>
+												<p>This operation completed successfully. This toast will close in 5 seconds.</p>
+											</div>,
+											{
+												timeout: 5000,
+											}
+										)
+									}
+								>
+									Toast success
+								</Button>
+								<Divider />
+								<Button
+									onPress={() =>
+										state.add(
+											<div className='flex flex-row items-center gap-2'>
+												<div className='rounded-full bg-danger p-1 flex'>
+													<Close size={20} />
+												</div>
+												<p>This operation failed. This toast will close in 5 seconds.</p>
+											</div>,
+											{
+												timeout: 5000,
+											}
+										)
+									}
+								>
+									Toast error
+								</Button>
+								<Divider />
+								<Button
+									onPress={() =>
+										state.add(
+											<div className='flex flex-row items-center gap-2'>
+												<div className='rounded-full bg-warning p-1 flex'>
+													<Warning size={20} />
+												</div>
+												<p>
+													This operation completes successfully, but there are some warnings. This toast will
+													close in 5 seconds.
+												</p>
+											</div>,
+											{
+												timeout: 5000,
+											}
+										)
+									}
+								>
+									Toast warning
+								</Button>
+								<Divider />
+								<Button
+									onPress={() =>
+										state.add(
+											<div className='flex flex-row items-center gap-2'>
+												<div className='rounded-full bg-primary p-1 flex'>
+													<InfoI size={20} />
+												</div>
+												<p>This is a new toast component. This toast will close in 5 seconds.</p>
+											</div>,
+											{
+												timeout: 5000,
+											}
+										)
+									}
+								>
+									Toast info
+								</Button>
+							</CardBody>
+						</Card>
+					)}
+				</ToastProvider>
 				<Card>
-					<CardBody className='gap-2'>
-						<Button size='sm' onPress={() => setIsOpenToastSuccess(true)}>
-							Toast success
-						</Button>
-						<Divider />
-						<Button size='sm' onPress={() => setIsOpenToastError(true)}>
-							Toast error
-						</Button>
-						<Divider />
-						<Button size='sm' onPress={() => setIsOpenToastWarning(true)}>
-							Toast warning
-						</Button>
-						<Divider />
+					<CardBody>
 						<Button size='sm' onPress={onOpen}>
 							dialog
 						</Button>
 					</CardBody>
 				</Card>
-				<Toast
-					isOpen={isOpenToastSuccess}
-					onClose={() => setIsOpenToastSuccess(false)}
-					message='This operation completed successfully. This toast will close in 5 seconds.'
-					startContent={
-						<div className='rounded-full bg-success p-1 flex'>
-							<Check size={20} />
-						</div>
-					}
-				/>
-				<Toast
-					isOpen={isOpenToastError}
-					onClose={() => setIsOpenToastError(false)}
-					message='This operation failed. This toast will close in 5 seconds.'
-					startContent={
-						<div className='rounded-full bg-danger p-1 flex'>
-							<Close size={20} />
-						</div>
-					}
-				/>
-				<Toast
-					isOpen={isOpenToastWarning}
-					onClose={() => setIsOpenToastWarning(false)}
-					message='This operation completes successfully, but there are some warnings. This toast will close in 5 seconds.'
-					startContent={
-						<div className='rounded-full bg-warning p-1 flex'>
-							<Exclamation size={20} />
-						</div>
-					}
-				/>
 				<Confirm title='Confirmation' message='Do you want to exit?' isOpen={isOpen} onClose={onClose} />
 			</MobileMain>
 		</div>

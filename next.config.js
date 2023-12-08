24d59ea98283654140bb5ce29bef083a1f5fe4ea
Jim Jin <jim_king_2000@163.com>
2023-12-08 15:40:22 +0800

    remove basePath

--- next.config.js
+++ next.config.js
@@ -9,14 +9,14 @@ const nextConfig = {
 	 *
 	 * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
 	 */
-	output: 'export',
+	// output: 'export',
 
 	/**
 	 * Set base path. This is usually the slug of your repository.
 	 *
 	 * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
 	 */
-	basePath: '/jujiuipcappui',
+	// basePath: '/jujiuipcappui',
 
 	/**
 	 * Disable server-based image optimization. Next.js does not support

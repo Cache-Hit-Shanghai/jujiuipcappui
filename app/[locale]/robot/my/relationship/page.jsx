9454ffc20977678cfa34a7761103f49f5c5635d5
Jim Jin <jim_king_2000@163.com>
2023-12-21 16:47:46 +0800

    refactory my/relationship

--- app/[locale]/robot/my/relationship/page.jsx
+++ app/[locale]/robot/my/relationship/page.jsx
@@ -2,20 +2,15 @@
 
 import {
 	Avatar,
+	User,
 	Tabs,
 	Tab,
-	User,
 	Badge,
 	Button,
 	Divider,
 	Card,
 	CardBody,
-	CardFooter,
 	Input,
-	Dropdown,
-	DropdownTrigger,
-	DropdownMenu,
-	DropdownItem,
 	Switch,
 	Modal,
 	ModalContent,
@@ -29,16 +24,59 @@ import {
 	MobileMain,
 	NavbarBack,
 	LabelButton,
-	LabeledControl,
 } from '@/jujiu-ui-components/nextui/core/core-ui';
 import { Confirm } from '@/jujiu-ui-components/nextui/extension/confirm';
-import Link, { useJuJiuT } from '@/state/translate';
-import { FavoriteBorder } from '@styled-icons/material/FavoriteBorder';
+import { useJuJiuT } from '@/state/translate';
 import { MoreHoriz } from '@styled-icons/material/MoreHoriz';
 import { Search } from '@styled-icons/material/Search';
-import { Edit } from '@styled-icons/material/Edit';
-import { DoNotDisturbOn } from '@styled-icons/material/DoNotDisturbOn';
-import { PersonRemove } from '@styled-icons/material/PersonRemove';
+
+function AddRemark({ isOpen, onOpenChange }) {
+	const t = useJuJiuT();
+
+	return (
+		<Modal
+			hideCloseButton
+			backdrop='blur'
+			placement='center'
+			isOpen={isOpen}
+			onOpenChange={onOpenChange}
+		>
+			<ModalContent className='gap-2'>
+				{(onClose) => (
+					<>
+						<ModalHeader className='flex flex-col gap-1'>
+							<p>{t('修改备注')}</p>
+							<p className='text-xs font-normal text-default-500'>
+								{t('最多不超过12个字')}
+							</p>
+						</ModalHeader>
+						<ModalBody>
+							<Input
+								isClearable
+								size='sm'
+								placeholder='Junior Garcia'
+								defaultValue='Junior Garcia'
+							/>
+						</ModalBody>
+						<ModalFooter>
+							<Button
+								size='sm'
+								color='danger'
+								variant='light'
+								onPress={onClose}
+							>
+								{t('取消')}
+							</Button>
+							<Button size='sm' color='primary' onPress={onClose}>
+								{t('确定')}
+							</Button>
+						</ModalFooter>
+					</>
+				)}
+			</ModalContent>
+		</Modal>
+	);
+}
 
 function FollowingItem({ children }) {
 	const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -62,47 +100,10 @@ function FollowingItem({ children }) {
 					isOpen={unfollowConfirm.isOpen}
 					onClose={unfollowConfirm.onClose}
 				/>
-				<Modal
-					hideCloseButton
-					backdrop='blur'
-					placement='center'
+				<AddRemark
 					isOpen={editRemark.isOpen}
 					onOpenChange={editRemark.onOpenChange}
-				>
-					<ModalContent className='gap-2'>
-						{(onClose) => (
-							<>
-								<ModalHeader className='flex flex-col gap-1'>
-									<p>{t('修改备注')}</p>
-									<p className='text-xs font-normal text-default-500'>
-										{t('最多不超过12个字')}
-									</p>
-								</ModalHeader>
-								<ModalBody>
-									<Input
-										isClearable
-										size='sm'
-										placeholder='Junior Garcia'
-										defaultValue='Junior Garcia'
-									/>
-								</ModalBody>
-								<ModalFooter>
-									<Button
-										size='sm'
-										color='danger'
-										variant='light'
-										onPress={onClose}
-									>
-										{t('取消')}
-									</Button>
-									<Button size='sm' color='primary' onPress={onClose}>
-										{t('确定')}
-									</Button>
-								</ModalFooter>
-							</>
-						)}
-					</ModalContent>
-				</Modal>
+				/>
 				<Modal
 					backdrop='blur'
 					placement='bottom'
@@ -151,46 +152,62 @@ function FollowingItem({ children }) {
 }
 
 function FriendItem({ children }) {
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+	const unfollowConfirm = useDisclosure();
+	const editRemark = useDisclosure();
 	const t = useJuJiuT();
 
 	return (
 		<div className='flex flex-row items-center justify-between'>
 			{children}
 			<div className='flex flex-row items-center gap-2'>
-				<Button size='sm'>发私信</Button>
-				<Dropdown backdrop='blur'>
-					<DropdownTrigger>
-						<Button isIconOnly variant='light'>
-							<MoreHoriz size={24} />
-						</Button>
-					</DropdownTrigger>
-					<DropdownMenu variant='faded' aria-label='Static Actions'>
-						<DropdownItem
-							showDivider
-							key='nickname'
-							endContent={<Edit size={24} />}
-						>
-							设置备注
-						</DropdownItem>
-						<DropdownItem key='noaccess' endContent={<Switch />}>
-							不让他看作品
-						</DropdownItem>
-						<DropdownItem showDivider key='filterout' endContent={<Switch />}>
-							不看他的作品
-						</DropdownItem>
-						<DropdownItem key='breakup' endContent={<PersonRemove size={24} />}>
-							移除朋友
-						</DropdownItem>
-						<DropdownItem
-							key='delete'
-							className='text-danger'
-							color='danger'
-							endContent={<DoNotDisturbOn size={24} />}
-						>
-							{t('取消关注')}
-						</DropdownItem>
-					</DropdownMenu>
-				</Dropdown>
+				<Button size='sm'>{t('发私信')}</Button>
+				<Button isIconOnly variant='light' onPress={onOpen}>
+					<MoreHoriz size={24} />
+				</Button>
+				<AddRemark
+					isOpen={editRemark.isOpen}
+					onOpenChange={editRemark.onOpenChange}
+				/>
+				<Confirm
+					message={t('确认不再关注？')}
+					isOpen={unfollowConfirm.isOpen}
+					onClose={unfollowConfirm.onClose}
+				/>
+				<Modal
+					backdrop='blur'
+					placement='bottom'
+					hideCloseButton
+					isOpen={isOpen}
+					onOpenChange={onOpenChange}
+					className='bg-transparent'
+				>
+					<ModalContent className='gap-2'>
+						{() => (
+							<>
+								<Card>
+									<CardBody>
+										<LabelButton
+											label={t('设置备注名')}
+											onPress={editRemark.onOpen}
+										/>
+									</CardBody>
+								</Card>
+								<Card>
+									<CardBody className='gap-2'>
+										<Button
+											size='sm'
+											color='danger'
+											onPress={unfollowConfirm.onOpen}
+										>
+											{t('取消关注')}
+										</Button>
+									</CardBody>
+								</Card>
+							</>
+						)}
+					</ModalContent>
+				</Modal>
 			</div>
 		</div>
 	);
@@ -212,76 +229,50 @@ export default function Page() {
 				>
 					<Tab key='friends' title={t('朋友')}>
 						<div className='flex flex-col gap-3'>
-							<Input
-								size='sm'
-								startContent={<Search size={24} />}
-								placeholder={t('{number}位朋友', { number: 5 })}
-							/>
 							<FriendItem>
-								<div className='flex flex-row items-center gap-2'>
-									<Badge
-										content=''
-										color='success'
-										shape='circle'
-										placement='bottom-right'
-									>
-										<Avatar src='https://avatars.githubusercontent.com/u/30373425?v=4' />
-									</Badge>
-									<div>
-										<p className='text-sm'>Junior Garcia</p>
-										<p className='text-xs text-default-500'>{t('在线')}</p>
-									</div>
-								</div>
+								<User
+									name='Junior Garcia'
+									description='C语言专家'
+									avatarProps={{
+										src: 'https://avatars.githubusercontent.com/u/30373425?v=4',
+									}}
+								/>
 							</FriendItem>
 							<FriendItem>
-								<div className='flex flex-row items-center gap-2'>
-									<Badge
-										content=''
-										color='success'
-										shape='circle'
-										placement='bottom-right'
-									>
-										<Avatar src='https://i.pravatar.cc/150?u=a04258114e29026702d' />
-									</Badge>
-									<div>
-										<p className='text-sm'>Jane Doe</p>
-										<p className='text-xs text-default-500'>{t('在线')}</p>
-									</div>
-								</div>
+								<User
+									name='Jane Doe'
+									description='UI设计师'
+									avatarProps={{
+										src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
+									}}
+								/>
 							</FriendItem>
 							<FriendItem>
-								<div className='flex flex-row items-center gap-2'>
-									<Badge
-										content=''
-										color='success'
-										shape='circle'
-										placement='bottom-right'
-									>
-										<Avatar src='https://i.pravatar.cc/150?u=a042581f4e29026024d' />
-									</Badge>
-									<div>
-										<p className='text-sm'>Junior Lee</p>
-										<p className='text-xs text-default-500'>{t('在线')}</p>
-									</div>
-								</div>
+								<User
+									name='Junior Lee'
+									description='专业金融分析师'
+									avatarProps={{
+										src: 'https://i.pravatar.cc/150?u=a042581f4e29026024d',
+									}}
+								/>
 							</FriendItem>
 							<FriendItem>
-								<div className='flex flex-row items-center gap-2'>
-									<Avatar src='https://i.pravatar.cc/150?u=a04258a2462d826712d' />
-									<div>
-										<p className='text-sm'>Tom Chen</p>
-										<p className='text-xs text-default-500'>{t('离线')}</p>
-									</div>
-								</div>
+								<User
+									name='Tom Chen'
+									description='舞蹈达人'
+									avatarProps={{
+										src: 'https://i.pravatar.cc/150?u=a04258a2462d826712d',
+									}}
+								/>
 							</FriendItem>
 							<FriendItem>
-								<div className='flex flex-row items-center gap-2'>
-									<Avatar src='https://i.pravatar.cc/150?u=a042581f4e29026704d' />
-									<div>
-										<p className='text-sm'>Lisa Wang</p>
-										<p className='text-xs text-default-500'>{t('离线')}</p>
-									</div>
-								</div>
+								<User
+									name='Lisa Wang'
+									description='an artist from Sydney'
+									avatarProps={{
+										src: 'https://i.pravatar.cc/150?u=a042581f4e29026704d',
+									}}
+								/>
 							</FriendItem>
 						</div>
 					</Tab>
@@ -296,37 +287,31 @@ export default function Page() {
 								{t('我的关注({number})', { number: 3 })}
 							</p>
 							<FollowingItem>
-								<div className='flex flex-row items-center gap-2'>
-									<Avatar src='https://avatars.githubusercontent.com/u/30373425?v=4' />
-									<div>
-										<p className='text-sm'>Junior Garcia</p>
-										<p className='text-xs text-default-500'>
-											{t('{number}篇笔记未看', { number: 2 })}
-										</p>
-									</div>
-								</div>
+								<User
+									name='Junior Garcia'
+									description={t('{number}篇笔记未看', { number: 2 })}
+									avatarProps={{
+										src: 'https://avatars.githubusercontent.com/u/30373425?v=4',
+									}}
+								/>
 							</FollowingItem>
 							<FollowingItem>
-								<div className='flex flex-row items-center gap-2'>
-									<Avatar src='https://i.pravatar.cc/150?u=a04258a2462d826712d' />
-									<div>
-										<p className='text-sm'>Tom Chen</p>
-										<p className='text-xs text-default-500'>
-											{t('{number}篇笔记未看', { number: 5 })}
-										</p>
-									</div>
-								</div>
+								<User
+									name='Tom Chen'
+									description={t('{number}篇笔记未看', { number: 5 })}
+									avatarProps={{
+										src: 'https://i.pravatar.cc/150?u=a04258a2462d826712d',
+									}}
+								/>
 							</FollowingItem>
 							<FollowingItem>
-								<div className='flex flex-row items-center gap-2'>
-									<Avatar src='https://i.pravatar.cc/150?u=a042581f4e29026704d' />
-									<div>
-										<p className='text-sm'>Lisa Wang</p>
-										<p className='text-xs text-default-500'>
-											{t('{number}篇笔记未看', { number: 20 })}
-										</p>
-									</div>
-								</div>
+								<User
+									name='Lisa Wang'
+									description={t('{number}篇笔记未看', { number: 20 })}
+									avatarProps={{
+										src: 'https://i.pravatar.cc/150?u=a042581f4e29026704d',
+									}}
+								/>
 							</FollowingItem>
 						</div>
 					</Tab>

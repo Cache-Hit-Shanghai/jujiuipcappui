644fab2581f3368d93b54aae274de77e4ddf0a30
Jim Jin <jim_king_2000@163.com>
2023-12-29 18:58:55 +0800

    refactory

--- app/[locale]/robot/my/idCard/page.jsx
+++ app/[locale]/robot/my/idCard/page.jsx
@@ -55,7 +55,7 @@ function PetIdCard({ src, name, gender, birthday, breed }) {
 			<CardBody className='flex flex-row gap-4'>
 				<input
 					type='file'
-					accept='image/jpeg,image/jpg,image/png,image/svg'
+					accept='image/*;'
 					id='file'
 					ref={inputFile}
 					className='hidden'
